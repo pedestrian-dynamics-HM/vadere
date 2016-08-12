e816e0da144497cd7967676b71ab4410bee47a42
Mario Teixeira Parente <mario.teixeira_parente@hm.edu>
2016-08-12 17:13:49 +0200

    Implements the logic newly

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianEvacuationTimeProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianEvacuationTimeProcessor.java
@@ -1,10 +1,10 @@
 package org.vadere.simulator.projects.dataprocessing_mtp;
 
 import org.vadere.simulator.control.SimulationState;
+import org.vadere.state.scenario.Pedestrian;
 
 public class PedestrianEvacuationTimeProcessor extends Processor<PedestrianIdDataKey, Double> {
 	private PedestrianStartTimeProcessor pedStTimeProc;
-	private PedestrianEndTimeProcessor pedEndTimeProc;
 
 	public PedestrianEvacuationTimeProcessor() {
 		super("tevac");
@@ -13,14 +13,22 @@ public class PedestrianEvacuationTimeProcessor extends Processor<PedestrianIdDat
 	@Override
 	protected void doUpdate(final SimulationState state) {
 		this.pedStTimeProc.update(state);
-		this.pedEndTimeProc.update(state);
 
-		this.pedEndTimeProc.getKeys().forEach(
-				key -> this.setValue(key, this.pedEndTimeProc.getValue(key) - this.pedStTimeProc.getValue(key)));
+		state.getTopography().getElements(Pedestrian.class).stream()
+				.map(ped -> new PedestrianIdDataKey(ped.getId()))
+				.forEach(key -> this.setValue(key, state.getSimTimeInSec() - this.pedStTimeProc.getValue(key)));
+	}
+
+	@Override
+	public void postLoop(final SimulationState state) {
+		state.getTopography().getElements(Pedestrian.class).stream()
+				.map(ped -> new PedestrianIdDataKey(ped.getId()))
+				.forEach(key -> this.setValue(key, Double.NaN));
 	}
 
 	@Override
 	void init(final AttributesProcessor attributes, final ProcessorFactory factory) {
-		// TODO [priority=medium] [task=check] is initialization needed?
+		AttributesPedestrianEvacuationTimeProcessor att = (AttributesPedestrianEvacuationTimeProcessor) attributes;
+		this.pedStTimeProc = (PedestrianStartTimeProcessor) factory.getProcessor(att.getPedestrianStartTimeProcessorId());
 	}
 }
