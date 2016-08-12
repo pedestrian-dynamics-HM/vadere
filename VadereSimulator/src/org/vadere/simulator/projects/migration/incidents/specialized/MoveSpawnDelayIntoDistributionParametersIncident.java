a1bec06baa4d3215990e1c5fa803ca43d0d10000
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-08-13 00:10:03 +0200

    implement new specialized Incident

--- VadereSimulator/src/org/vadere/simulator/projects/migration/incidents/specialized/MoveSpawnDelayIntoDistributionParametersIncident.java
+++ VadereSimulator/src/org/vadere/simulator/projects/migration/incidents/specialized/MoveSpawnDelayIntoDistributionParametersIncident.java
@@ -1,8 +1,15 @@
 package org.vadere.simulator.projects.migration.incidents.specialized;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.vadere.simulator.projects.io.JsonConverter;
 import org.vadere.simulator.projects.migration.Graph;
 import org.vadere.simulator.projects.migration.MigrationException;
 import org.vadere.simulator.projects.migration.incidents.Incident;
+import org.vadere.state.scenario.ConstantDistribution;
+
+import static org.vadere.simulator.projects.migration.IncidentDatabase.path;
 
 
 public class MoveSpawnDelayIntoDistributionParametersIncident extends Incident {
@@ -15,7 +22,22 @@ public class MoveSpawnDelayIntoDistributionParametersIncident extends Incident {
 	@Override
 	public void resolve(Graph graph, StringBuilder log) throws MigrationException {
 
+		Graph.Node sources_node = graph.getNodeByPath(path("vadere", "topography", "sources"));// null-check topography as well? must exit always, no?
 
+		if (sources_node != null) {
+			for (JsonNode source : sources_node.getJsonNode()) {
+				if (source.has("spawnDelay")) {
+
+					double spawnDelay = source.get("spawnDelay").asDouble();
+
+					if (source.get("interSpawnTimeDistribution").asText().equals(ConstantDistribution.class.getName()) && spawnDelay != -1.0) {
+						((ObjectNode) source).set("distributionParameters", JsonConverter.toJsonNode(new Double[] {spawnDelay}));
+					}
+
+					((ObjectNode) source).remove("spawnDelay");
+				}
+			}
+		}
 	}
 
 }
