d7f824fa247d543f44424ba5e3e425ae758fbb3e
Mario Teixeira Parente <mario.teixeira_parente@hm.edu>
2016-08-12 17:12:29 +0200

    Added postLoop method

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianEndTimeProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianEndTimeProcessor.java
@@ -1,41 +1,32 @@
 package org.vadere.simulator.projects.dataprocessing_mtp;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.vadere.simulator.control.SimulationState;
 import org.vadere.state.scenario.Pedestrian;
 
+import java.util.Collection;
+
 public class PedestrianEndTimeProcessor extends Processor<PedestrianIdDataKey, Double> {
-	private PedestrianStartTimeProcessor pedStTimeProc;
+    public PedestrianEndTimeProcessor() {
+        super("tend");
+    }
 
-	public PedestrianEndTimeProcessor() {
-		super("tend");
+    @Override
+    protected void doUpdate(final SimulationState state) {
+        this.setValues(state.getTopography().getElements(Pedestrian.class), state.getSimTimeInSec());
+    }
 
-		this.pedStTimeProc = pedStTimeProc;
-	}
+    @Override
+    public void postLoop(final SimulationState state) {
+        this.setValues(state.getTopography().getElements(Pedestrian.class), Double.NaN);
+    }
 
-	@Override
-	protected void doUpdate(final SimulationState state) {
-		this.pedStTimeProc.update(state);
+    @Override
+    void init(final AttributesProcessor attributes, final ProcessorFactory factory) {
+        // No initialization needed
+    }
 
-		Set<PedestrianIdDataKey> stTimeKeys = new HashSet<>();
-		stTimeKeys.addAll(this.pedStTimeProc.getKeys()); // Create a copy of start time keys
-
-		Set<PedestrianIdDataKey> currentKeys = state.getTopography().getElements(Pedestrian.class).stream()
-				.map(ped -> new PedestrianIdDataKey(ped.getId())).collect(Collectors.toSet());
-		Set<PedestrianIdDataKey> endTimeKeys = this.getKeys();
-
-		// Find out which pedestrians have been exited in this step
-		stTimeKeys.removeAll(currentKeys);
-		stTimeKeys.removeAll(endTimeKeys);
-
-		stTimeKeys.forEach(key -> this.setValue(key, state.getSimTimeInSec()));
-	}
-
-	@Override
-	void init(final AttributesProcessor attributes, final ProcessorFactory factory) {
-		// TODO [priority=medium] [task=check] is initialization needed?
-	}
+    private void setValues(Collection<Pedestrian> peds, double value) {
+        peds.stream().map(ped -> new PedestrianIdDataKey(ped.getId()))
+                .forEach(key -> this.setValue(key, value));
+    }
 }
