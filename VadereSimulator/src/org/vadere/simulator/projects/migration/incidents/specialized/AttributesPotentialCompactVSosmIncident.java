136efb5213f8f2e5b50c3b9bafbfaa67abdbb550
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-08-14 12:52:37 +0200

    follow camelCase convention for variable names

--- VadereSimulator/src/org/vadere/simulator/projects/migration/incidents/specialized/AttributesPotentialCompactVSosmIncident.java
+++ VadereSimulator/src/org/vadere/simulator/projects/migration/incidents/specialized/AttributesPotentialCompactVSosmIncident.java
@@ -29,35 +29,35 @@ public class AttributesPotentialCompactVSosmIncident extends Incident {
 	@Override
 	public void resolve(Graph graph, StringBuilder log) throws MigrationException {
 
-		Graph.Node AttributesOSM_node = graph.getNodeByPath(path("vadere", "attributesModel", AttributesOSM.class.getName()));
+		Graph.Node attributesOSMnode = graph.getNodeByPath(path("vadere", "attributesModel", AttributesOSM.class.getName()));
 
-		if (AttributesOSM_node != null) {
-			JsonNode AttributesOSM_JsonNode = AttributesOSM_node.getJsonNode();
+		if (attributesOSMnode != null) {
+			JsonNode attributesOSMjsonNode = attributesOSMnode.getJsonNode();
 
-			Graph.Node AttributesPotentialCompact_node = graph.getNodeByPath(path("vadere", "attributesModel", AttributesPotentialCompact.class.getName()));
-			Graph.Node AttributesPotentialOSM_node = graph.getNodeByPath(path("vadere", "attributesModel", AttributesPotentialOSM.class.getName()));
+			Graph.Node attributesPotentialCompactNode = graph.getNodeByPath(path("vadere", "attributesModel", AttributesPotentialCompact.class.getName()));
+			Graph.Node attributesPotentialOSMnode = graph.getNodeByPath(path("vadere", "attributesModel", AttributesPotentialOSM.class.getName()));
 
-			if (AttributesPotentialCompact_node != null && AttributesPotentialOSM_node != null) {
+			if (attributesPotentialCompactNode != null && attributesPotentialOSMnode != null) {
 				throw new MigrationException(this, "[AttributesPotentialCompact] and [AttributesPotentialOSM] are both present, that is not allowed.");
 			}
 
-			String beforeChange = AttributesOSM_JsonNode.toString();
+			String beforeChange = attributesOSMjsonNode.toString();
 
-			if (AttributesPotentialCompact_node != null) {
-				((ObjectNode) AttributesOSM_JsonNode).put("pedestrianPotentialModel", PotentialFieldPedestrianCompact.class.getName());
-				((ObjectNode) AttributesOSM_JsonNode).put("obstaclePotentialModel", PotentialFieldObstacleCompact.class.getName());
+			if (attributesPotentialCompactNode != null) {
+				((ObjectNode) attributesOSMjsonNode).put("pedestrianPotentialModel", PotentialFieldPedestrianCompact.class.getName());
+				((ObjectNode) attributesOSMjsonNode).put("obstaclePotentialModel", PotentialFieldObstacleCompact.class.getName());
 
-				if (!beforeChange.equals(AttributesOSM_JsonNode.toString())) {
+				if (!beforeChange.equals(attributesOSMjsonNode.toString())) {
 					log.append("\t- AttributesOSM: since AttributesPotentialCompact is present, set [pedestrianPotentialModel] to PotentialFieldPedestrianCompact " +
 							"and [obstaclePotentialModel] to PotentialFieldObstacleCompact" + "\n");
 				}
 			}
 
-			if (AttributesPotentialOSM_node != null) {
-				((ObjectNode) AttributesOSM_JsonNode).put("pedestrianPotentialModel", PotentialFieldPedestrianOSM.class.getName());
-				((ObjectNode) AttributesOSM_JsonNode).put("obstaclePotentialModel", PotentialFieldObstacleOSM.class.getName());
+			if (attributesPotentialOSMnode != null) {
+				((ObjectNode) attributesOSMjsonNode).put("pedestrianPotentialModel", PotentialFieldPedestrianOSM.class.getName());
+				((ObjectNode) attributesOSMjsonNode).put("obstaclePotentialModel", PotentialFieldObstacleOSM.class.getName());
 
-				if (!beforeChange.equals(AttributesOSM_JsonNode.toString())) {
+				if (!beforeChange.equals(attributesOSMjsonNode.toString())) {
 					log.append("\t- AttributesOSM: since AttributesPotentialOSM is present, set [pedestrianPotentialModel] to PotentialFieldPedestrianOSM " +
 							"and [obstaclePotentialModel] to PotentialFieldObstacleOSM" + "\n");
 				}
