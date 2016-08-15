6eb255c193306b24aa1f37ad5bcf137f8c156295
Mario Teixeira Parente <mario.teixeira_parente@hm.edu>
2016-08-15 14:55:38 +0200

    Adds flow processor

---
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/AttributesPedestrianFlowProcessor.java
@@ -0,0 +1,14 @@
+package org.vadere.simulator.projects.dataprocessing_mtp;
+
+public class AttributesPedestrianFlowProcessor extends AttributesProcessor {
+    private int pedestrianVelocityProcessorId;
+    private int pedestrianDensityProcessorId;
+
+    public int getPedestrianVelocityProcessorId() {
+        return this.pedestrianVelocityProcessorId;
+    }
+
+    public int getPedestrianDensityProcessorId() {
+        return this.pedestrianDensityProcessorId;
+    }
+}
---
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianFlowProcessor.java
@@ -0,0 +1,38 @@
+package org.vadere.simulator.projects.dataprocessing_mtp;
+
+import org.vadere.simulator.control.SimulationState;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PedestrianFlowProcessor extends Processor<TimestepPedestrianIdDataKey, Double> {
+    private PedestrianVelocityProcessor pedVelProc;
+    private PedestrianDensityProcessor pedDensProc;
+
+    public PedestrianFlowProcessor() {
+        super("flow");
+    }
+
+    @Override
+    protected void doUpdate(final SimulationState state) {
+        this.pedVelProc.update(state);
+        this.pedDensProc.update(state);
+
+        Set<TimestepPedestrianIdDataKey> keys = this.pedVelProc.getKeys().stream().filter(key -> key.getTimestep() == state.getStep()).collect(Collectors.toSet());
+
+        for (TimestepPedestrianIdDataKey key : keys) {
+            double velocity = this.pedVelProc.getValue(key);
+            double density = this.pedDensProc.getValue(key);
+
+            this.setValue(key, velocity * density);
+        }
+    }
+
+    @Override
+    void init(final AttributesProcessor attributes, final ProcessorFactory factory) {
+        AttributesPedestrianFlowProcessor att = (AttributesPedestrianFlowProcessor) attributes;
+
+        this.pedVelProc = (PedestrianVelocityProcessor) factory.getProcessor(att.getPedestrianVelocityProcessorId());
+        this.pedDensProc = (PedestrianDensityProcessor) factory.getProcessor(att.getPedestrianDensityProcessorId());
+    }
+}
