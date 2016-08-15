e3d4a0cd5e2e37da3d606edd82879bb454ca71f5
Mario Teixeira Parente <mario.teixeira_parente@hm.edu>
2016-08-15 17:05:34 +0200

    Adds waiting time processor

---
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianWaitingTimeProcessor.java
@@ -0,0 +1,43 @@
+package org.vadere.simulator.projects.dataprocessing_mtp;
+
+import org.vadere.simulator.control.SimulationState;
+import org.vadere.util.geometry.shapes.VPoint;
+import org.vadere.util.geometry.shapes.VRectangle;
+
+import java.util.Map;
+
+public class PedestrianWaitingTimeProcessor extends Processor<PedestrianIdDataKey, Double> {
+    private double lastSimTime;
+    private VRectangle waitingArea;
+
+    public PedestrianWaitingTimeProcessor() {
+        super("twait");
+
+        this.lastSimTime = 0.0;
+    }
+
+    @Override
+    protected void doUpdate(final SimulationState state) {
+        Map<Integer, VPoint> pedPosMap = state.getPedestrainPositionMap();
+
+        double dt = state.getSimTimeInSec() - this.lastSimTime;
+
+        for (Map.Entry<Integer, VPoint> entry : pedPosMap.entrySet()) {
+            int pedId = entry.getKey();
+            VPoint pos = entry.getValue();
+
+            if (this.waitingArea.contains(pos)) {
+                PedestrianIdDataKey key = new PedestrianIdDataKey(pedId);
+                this.setValue(key, (this.hasValue(key) ? this.getValue(key) : 0.0) + dt);
+            }
+        }
+
+        this.lastSimTime = state.getSimTimeInSec();
+    }
+
+    @Override
+    void init(final AttributesProcessor attributes, final ProcessorFactory factory) {
+        AttributesPedestrianWaitingTimeProcessor att = (AttributesPedestrianWaitingTimeProcessor) attributes;
+        this.waitingArea = att.getWaitingArea();
+    }
+}
