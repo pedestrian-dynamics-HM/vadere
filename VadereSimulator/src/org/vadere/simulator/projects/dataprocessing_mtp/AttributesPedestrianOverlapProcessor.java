46c78fc02865a20aa29b69df7867ea8c70780033
Mario Teixeira Parente <mario.teixeira_parente@hm.edu>
2016-08-15 16:32:35 +0200

    Adds overlap processor

---
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/AttributesPedestrianOverlapProcessor.java
@@ -0,0 +1,9 @@
+package org.vadere.simulator.projects.dataprocessing_mtp;
+
+public class AttributesPedestrianOverlapProcessor extends AttributesProcessor {
+    private double pedRadius = 0.2;
+
+    public double getPedRadius() {
+        return pedRadius;
+    }
+}
---
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/PedestrianOverlapProcessor.java
@@ -0,0 +1,32 @@
+package org.vadere.simulator.projects.dataprocessing_mtp;
+
+import org.vadere.simulator.control.SimulationState;
+import org.vadere.util.geometry.shapes.VPoint;
+
+import java.util.Map;
+
+public class PedestrianOverlapProcessor extends Processor<TimestepPedestrianIdDataKey, Integer> {
+    private double pedRadius;
+
+    public PedestrianOverlapProcessor() {
+        super("overlaps");
+    }
+
+    @Override
+    protected void doUpdate(final SimulationState state) {
+        Map<Integer, VPoint> pedPosMap = state.getPedestrainPositionMap();
+
+        pedPosMap.entrySet().forEach(entry -> this.setValue(new TimestepPedestrianIdDataKey(state.getStep(), entry.getKey()), this.calculateOverlaps(pedPosMap, entry.getValue())));
+    }
+
+    @Override
+    void init(final AttributesProcessor attributes, final ProcessorFactory factory) {
+        AttributesPedestrianOverlapProcessor att = (AttributesPedestrianOverlapProcessor) attributes;
+
+        this.pedRadius = att.getPedRadius();
+    }
+
+    private int calculateOverlaps(final Map<Integer, VPoint> pedPosMap, VPoint pos) {
+        return (int) pedPosMap.values().stream().filter(pedPos -> pedPos.distance(pos) < 2*this.pedRadius).count();
+    }
+}
