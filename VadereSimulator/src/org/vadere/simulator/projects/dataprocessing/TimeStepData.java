df38613eb04b237a20f481a8bf2908480880e4b5
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-08-28 19:20:54 +0200

    delete unused classes tied to TimeStep

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/TimeStepData.java
+++
@@ -1,9 +0,0 @@
-package org.vadere.simulator.projects.dataprocessing;
-
-/**
- * Represents data that belongs to a given time step, like "speed at second 1".
- * 
- */
-public interface TimeStepData {
-
-}
