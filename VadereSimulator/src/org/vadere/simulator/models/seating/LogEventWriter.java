e9c659aa5dfbdb501bc9f82fecacb7767ad7ef51
Jakob Schöttl <jschoett@gmail.com>
2016-08-30 09:45:10 +0200

    Implement logging of sit-down events

--- VadereSimulator/src/org/vadere/simulator/models/seating/LogEventWriter.java
+++ VadereSimulator/src/org/vadere/simulator/models/seating/LogEventWriter.java
@@ -3,6 +3,9 @@ package org.vadere.simulator.models.seating;
 import java.io.OutputStream;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.vadere.simulator.control.SimulationState;
 import org.vadere.simulator.projects.dataprocessing.processors.Processor;
@@ -10,9 +13,15 @@ import org.vadere.simulator.projects.dataprocessing.writer.ProcessorWriter;
 import org.vadere.state.attributes.processors.AttributesWriter;
 import org.vadere.state.scenario.Pedestrian;
 
+/**
+ * Produce output in same format as LOG_EVENT.csv from the SeatingDataCollection app.
+ * 
+ * See *_EVENT constants for supported log event types.
+ *
+ */
 public class LogEventWriter extends ProcessorWriter {
 	
-	// These fields come from the enum LogEventType from the SeatingDataCollection app project:
+	// These fields come from the enum LogEventType from the SeatingDataCollection app:
 	private static final String INITIALIZATION_END_EVENT = "INITIALIZATION_END";
 	private static final String SIT_DOWN_EVENT = "SIT_DOWN";
 	
@@ -21,6 +30,7 @@ public class LogEventWriter extends ProcessorWriter {
 	private TrainModel trainModel;
 	private LocalTime time;
 	private int nextLogEventId = 1;
+	private List<Seat> emptySeats;
 
 	public LogEventWriter(OutputStream out, Processor processor, AttributesWriter attributes) {
 		super(out, processor, attributes);
@@ -31,10 +41,8 @@ public class LogEventWriter extends ProcessorWriter {
 	public void preLoop(SimulationState state) {
 		super.preLoop(state);
 
-		// TODO implement
-//		final SeatingModel seatingModel = (SeatingModel) state.getMainModel();
-//		trainModel = seatingModel.getTrainModel();
-
+		trainModel = getTrainModelFromSimulationState(state);
+		emptySeats = new LinkedList<>(trainModel.getSeats()); // will be filtered in writeNewSitDownEvents()
 		time = LocalTime.now();
 
 		writeInitialSitDownEvents();
@@ -48,20 +56,28 @@ public class LogEventWriter extends ProcessorWriter {
 		writeNewSitDownEvents();
 	}
 
+	private TrainModel getTrainModelFromSimulationState(SimulationState state) {
+		// TODO implement
+//		final SeatingModel seatingModel = (SeatingModel) state.getMainModel();
+		final SeatingModel seatingModel = null;
+		return seatingModel.getTrainModel();
+	}
+
 	private void writeInitialSitDownEvents() {
-		for (Seat seat : trainModel.getSeats()) {
-			Pedestrian person = seat.getSittingPerson();
-			if (person != null) {
-				// TODO this does not work yet: 2nd param must be the seat number from 1 to 16 within one compartment!
-				writeSitDownEvent(person, seat.getAssociatedTarget().getId());
-			}
-		}
+		writeNewSitDownEvents();
 	}
 
 	private void writeNewSitDownEvents() {
-		// TODO find persons that just sat down
-		//for each seat which was empty before:
-		//writeSitDownEvent(seat.getSittingPerson(), seatNumber);
+		Iterator<Seat> it = emptySeats.iterator();
+		while (it.hasNext()) {
+			final Seat seat = it.next();
+			final Pedestrian person = seat.getSittingPerson();
+			if (person != null) {
+				// TODO this does not work yet: 2nd param must be the seat number from 1 to 16 within one compartment!
+				writeSitDownEvent(person, seat.getAssociatedTarget().getId());
+				it.remove();
+			}
+		}
 	}
 
 	private void writeInitializationEndEvent() {
