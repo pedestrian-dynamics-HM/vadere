5f93c5bc82e043cc0ce037ee9995fb68bfd0f795
Jakob Schöttl <jschoett@gmail.com>
2016-09-01 16:55:34 +0200

    Bugfix: Implement hash and equals for keys

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/TimestepDataKey.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/TimestepDataKey.java
@@ -14,4 +14,26 @@ public class TimestepDataKey implements Comparable<TimestepDataKey> {
     public static String getHeader() {
         return "ts";
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + timestep;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TimestepDataKey other = (TimestepDataKey) obj;
+		if (timestep != other.timestep)
+			return false;
+		return true;
+	}
 }
--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/TimestepPedestrianIdDataKey.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing_mtp/TimestepPedestrianIdDataKey.java
@@ -29,4 +29,29 @@ public class TimestepPedestrianIdDataKey implements Comparable<TimestepPedestria
 	public static String[] getHeaders() {
 		return new String[] { TimestepDataKey.getHeader(), PedestrianIdDataKey.getHeader() };
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + pedestrianId;
+		result = prime * result + timestep;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TimestepPedestrianIdDataKey other = (TimestepPedestrianIdDataKey) obj;
+		if (pedestrianId != other.pedestrianId)
+			return false;
+		if (timestep != other.timestep)
+			return false;
+		return true;
+	}
 }
