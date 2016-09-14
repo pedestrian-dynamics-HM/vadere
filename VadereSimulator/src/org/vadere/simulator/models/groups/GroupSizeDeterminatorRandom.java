b9801d06ceb1688a4e07ec7910864cef09298f69
Jakob Schöttl <jschoett@gmail.com>
2016-09-14 17:11:49 +0200

    Replace algorithm with Apache Commons class

--- VadereSimulator/src/org/vadere/simulator/models/groups/GroupSizeDeterminatorRandom.java
+++ VadereSimulator/src/org/vadere/simulator/models/groups/GroupSizeDeterminatorRandom.java
@@ -3,48 +3,43 @@ package org.vadere.simulator.models.groups;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
+import org.apache.commons.math3.random.JDKRandomGenerator;
+import org.apache.commons.math3.random.RandomGenerator;
+
 public class GroupSizeDeterminatorRandom implements GroupSizeDeterminator {
 
-	private double[] probabilities;
-	private final Random random;
+	private EnumeratedIntegerDistribution distribution;
 
 	public GroupSizeDeterminatorRandom(List<Double> fractions, Random random) {
+		
+		// It's not clear from the API doc if the EnumeratedIntegerDistribution normalizes fractions.
+		// If it does, most of the following is unnecessary.
 
-		probabilities = new double[fractions.size()];
 		double sum = 0;
-
 		for (double i : fractions) {
 			sum += i;
 		}
 
 		if (sum == 0) {
-			throw new IllegalArgumentException(
-					"Sum of group size fractions must not be 0.");
+			throw new IllegalArgumentException("Sum of group size fractions must not be 0.");
 		}
+
+		final int[] groupSizeValues = new int[fractions.size()];
+		final double[] probabilities = new double[fractions.size()];
 
 		for (int i = 0; i < fractions.size(); i++) {
+			groupSizeValues[i] = i + 1;
 			probabilities[i] = fractions.get(i) / sum;
 		}
-
-		this.random = random;
+		
+		final RandomGenerator rng = new JDKRandomGenerator(random.nextInt());
+		distribution = new EnumeratedIntegerDistribution(rng, groupSizeValues, probabilities);
 	}
 
 	@Override
 	public int nextGroupSize() {
-		int result = 1;
-
-		double rand = random.nextDouble();
-		double sum = 0;
-
-		for (double prob : probabilities) {
-			sum += prob;
-			if (rand <= sum) {
-				break;
-			} else {
-				result++;
-			}
-		}
-		return result;
+		return distribution.sample();
 	}
 
 }
