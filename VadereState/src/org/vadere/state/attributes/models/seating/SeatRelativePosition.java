e5f7191e8c2d3ca35a1324646adcb4f5473b0a4f
Jakob Schöttl <jschoett@gmail.com>
2016-09-16 17:10:09 +0200

    Add some forgotten enums

---
+++ VadereState/src/org/vadere/state/attributes/models/seating/SeatRelativePosition.java
@@ -0,0 +1,5 @@
+package org.vadere.state.attributes.models.seating;
+
+public enum SeatRelativePosition {
+	ACROSS, DIAGONAL, NEXT;
+}
---
+++ VadereState/src/org/vadere/state/attributes/models/seating/SeatSide.java
@@ -0,0 +1,5 @@
+package org.vadere.state.attributes.models.seating;
+
+public enum SeatSide {
+	WINDOW, AISLE;
+}
