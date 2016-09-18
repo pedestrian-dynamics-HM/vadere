04745715500767c9697e2a02628ffef1b7d58ac0
Jakob Schöttl <jschoett@gmail.com>
2016-09-18 22:55:58 +0200

    Add mandatory hashCode method

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/datakey/IdDataKey.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/datakey/IdDataKey.java
@@ -22,6 +22,14 @@ public class IdDataKey implements Comparable<IdDataKey> {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
