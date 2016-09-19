3b22aef19658c68c4e033b64d0ff51cb01b420c3
Jakob Schöttl <jschoett@gmail.com>
2016-09-19 14:44:00 +0200

    Add attributes for log event processor

--- VadereState/src/org/vadere/state/attributes/processor/AttributesLogEventProcessor.java
+++ VadereState/src/org/vadere/state/attributes/processor/AttributesLogEventProcessor.java
@@ -3,9 +3,25 @@ package org.vadere.state.attributes.processor;
 public class AttributesLogEventProcessor extends AttributesProcessor {
 	
 	private int compartmentIndex = 0;
+	
+	private int surveyId = 1;
+	private int personIdOffset = 0;
+	private int firstLogEventId = 1;
 
 	public int getCompartmentIndex() {
 		return compartmentIndex;
+	}
+
+	public int getSurveyId() {
+		return surveyId;
+	}
+
+	public int getPersonIdOffset() {
+		return personIdOffset;
+	}
+
+	public int getFirstLogEventId() {
+		return firstLogEventId;
 	}
 
 }
