afec6b9ec0546cbd333995d9477a3b13273db460
Jakob Schöttl <jschoett@gmail.com>
2016-09-19 16:29:40 +0200

    Add JavaDoc for attributes

--- VadereState/src/org/vadere/state/attributes/processor/AttributesLogEventProcessor.java
+++ VadereState/src/org/vadere/state/attributes/processor/AttributesLogEventProcessor.java
@@ -5,7 +5,15 @@ public class AttributesLogEventProcessor extends AttributesProcessor {
 	private int compartmentIndex = 0;
 	
 	private int surveyId = 1;
+	/**
+	 * Offset to add to the pedestrian ID in order to prevent duplicate person
+	 * IDs from different log event processors.
+	 */
 	private int personIdOffset = 0;
+	/**
+	 * Log event ID for initializing the ID counter to prevent duplicate log
+	 * event IDs from different log event processors.
+	 */
 	private int firstLogEventId = 1;
 
 	public int getCompartmentIndex() {
