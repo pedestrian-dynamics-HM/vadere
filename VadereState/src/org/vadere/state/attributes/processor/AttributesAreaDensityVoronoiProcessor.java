941e9b5f8cb80587cc258dbd6da3609d8594f133
Mario Teixeira Parente <mario.teixeira_parente@hm.edu>
2016-09-22 13:21:49 +0200

    Add @author to processors' attributes files for Mario Teixeira Parente

--- VadereState/src/org/vadere/state/attributes/processor/AttributesAreaDensityVoronoiProcessor.java
+++ VadereState/src/org/vadere/state/attributes/processor/AttributesAreaDensityVoronoiProcessor.java
@@ -2,6 +2,11 @@ package org.vadere.state.attributes.processor;
 
 import org.vadere.util.geometry.shapes.VRectangle;
 
+/**
+ * @author Mario Teixeira Parente
+ *
+ */
+
 public class AttributesAreaDensityVoronoiProcessor extends AttributesAreaProcessor {
     private VRectangle voronoiArea = new VRectangle(0, 0, 1, 1);
 
