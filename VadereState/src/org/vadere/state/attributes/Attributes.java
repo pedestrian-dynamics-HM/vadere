84dae007562e650364efb25d6f04e526f3ef1b8b
Jakob Schöttl <jschoett@gmail.com>
2016-11-04 18:54:12 +0100

    Add doc to Attributes

--- VadereState/src/org/vadere/state/attributes/Attributes.java
+++ VadereState/src/org/vadere/state/attributes/Attributes.java
@@ -11,6 +11,14 @@ import com.google.gson.JsonElement;
  * based methods to convert the fields and values of the Attributes classes from
  * and to key-value store.
  * 
+ * Attributes are "static" parameters i.e. they are immutable while a simulation
+ * is running. This is implemented by deriving from the
+ * {@link org.apache.commons.attributes.Sealable} interface. Attributes can have
+ * setters, but the setters must call the {@code checkSealed()} method before
+ * changing a value! In addition, if an attributes class contains other
+ * attributes classes as fields, it must override {@link #seal()} to also seal
+ * these objects. All other fields must be immutable (e.g. String, Double,
+ * VPoint,...).
  * 
  */
 public abstract class Attributes extends DefaultSealable {
