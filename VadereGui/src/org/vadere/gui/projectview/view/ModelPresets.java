b523ea4c62a829f4d81c36a224b2ddd448ddf721
Felix Dietrich <felix.dietrich@tum.de>
2016-12-12 13:22:31 +0100

    Added model presets for GNM, SFM, and OVM.

--- VadereGui/src/org/vadere/gui/projectview/view/ModelPresets.java
+++ VadereGui/src/org/vadere/gui/projectview/view/ModelPresets.java
@@ -5,13 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.vadere.simulator.models.MainModel;
+import org.vadere.simulator.models.gnm.GradientNavigationModel;
 import org.vadere.simulator.models.osm.OptimalStepsModel;
+import org.vadere.simulator.models.ovm.OptimalVelocityModel;
+import org.vadere.simulator.models.sfm.SocialForceModel;
 import org.vadere.state.attributes.Attributes;
 import org.vadere.state.attributes.ModelDefinition;
 import org.vadere.state.attributes.models.AttributesFloorField;
+import org.vadere.state.attributes.models.AttributesGNM;
 import org.vadere.state.attributes.models.AttributesOSM;
+import org.vadere.state.attributes.models.AttributesOVM;
 import org.vadere.state.attributes.models.AttributesPotentialCompact;
-import org.vadere.state.attributes.models.AttributesPotentialOSM;
+import org.vadere.state.attributes.models.AttributesPotentialGNM;
+import org.vadere.state.attributes.models.AttributesPotentialSFM;
+import org.vadere.state.attributes.models.AttributesSFM;
 
 /**
  * Singleton for model presets.
@@ -39,7 +46,21 @@ public class ModelPresets {
 		list.add(AttributesFloorField.class);
 		registerModelPreset(OptimalStepsModel.class, list);
 
+		list.clear();
+		list.add(AttributesGNM.class);
+		list.add(AttributesPotentialGNM.class);
+		list.add(AttributesFloorField.class);
+		registerModelPreset(GradientNavigationModel.class, list);
 
+		list.clear();
+		list.add(AttributesSFM.class);
+		list.add(AttributesPotentialSFM.class);
+		list.add(AttributesFloorField.class);
+		registerModelPreset(SocialForceModel.class, list);
+		
+		list.clear();
+		list.add(AttributesOVM.class);
+		registerModelPreset(OptimalVelocityModel.class, list);
 
 		// list.clear();
 		// list.add(...);
