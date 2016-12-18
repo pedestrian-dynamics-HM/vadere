4d73d2c7f825cff9ee87505321a4694dcdcf35b2
Jakob Schöttl <jschoett@gmail.com>
2016-12-18 18:24:52 +0100

    Remove unused dummy attributes

--- VadereState/src/org/vadere/state/attributes/models/AttributesReynolds.java
+++ VadereState/src/org/vadere/state/attributes/models/AttributesReynolds.java
@@ -3,14 +3,5 @@ package org.vadere.state.attributes.models;
 import org.vadere.state.attributes.Attributes;
 
 public class AttributesReynolds extends Attributes {
-
-	private int dummy = 1;
-
-	public AttributesReynolds() {}
-
-	// Getters...
-	public int getDummy() {
-		return dummy;
-	}
-
+	// no attributes yet
 }
