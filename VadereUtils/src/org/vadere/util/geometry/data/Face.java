18c5286b01cdea785a7e0ae2e1c8fac33aefabd3
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2017-01-30 10:20:15 +0100

    refactoring of the triangulation code

--- VadereUtils/src/org/vadere/util/geometry/data/Face.java
+++ VadereUtils/src/org/vadere/util/geometry/data/Face.java
@@ -3,11 +3,16 @@ package org.vadere.util.geometry.data;
 import org.jetbrains.annotations.NotNull;
 import org.vadere.util.geometry.GeometryUtils;
 import org.vadere.util.geometry.shapes.IPoint;
+import org.vadere.util.geometry.shapes.MLine;
+import org.vadere.util.geometry.shapes.VLine;
+import org.vadere.util.geometry.shapes.VPoint;
 import org.vadere.util.geometry.shapes.VPolygon;
+import org.vadere.util.geometry.shapes.VTriangle;
 
 import java.awt.geom.Path2D;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -18,6 +23,23 @@ import java.util.stream.StreamSupport;
  * @param <P> the type of the coordinates the face uses.
  */
 public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
+
+	public static <P extends IPoint> Face<P> of(P p1, P p2, P p3) {
+		Face superTriangle = new Face();
+		HalfEdge edge1 = new HalfEdge(p1, superTriangle);
+		HalfEdge edge2 = new HalfEdge(p2, superTriangle);
+		HalfEdge edge3 = new HalfEdge(p3, superTriangle);
+		edge1.setNext(edge2);
+		edge2.setNext(edge3);
+		edge3.setNext(edge1);
+		superTriangle.setEdge(edge1);
+		return superTriangle;
+	}
+
+
+	public static <P extends IPoint> Face<P> getBorder(Class<P> p) {
+		return new Face<>();
+	}
 
 	/**
 	 * One of the half-edges bordering this face.
@@ -40,24 +62,16 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 	 */
 	public Face() {}
 
-	public static <P extends IPoint> Face<P> of(P p1, P p2, P p3) {
-		Face superTriangle = new Face();
-		HalfEdge edge1 = new HalfEdge(p1, superTriangle);
-		HalfEdge edge2 = new HalfEdge(p2, superTriangle);
-		HalfEdge edge3 = new HalfEdge(p3, superTriangle);
-		edge1.setNext(edge2);
-		edge2.setNext(edge3);
-		edge3.setNext(edge1);
-		superTriangle.setEdge(edge1);
-		return superTriangle;
-	}
-
 	/**
 	 * Sets one of the half-edges bordering this face.
 	 * @param edge half-edge bordering this face
 	 */
 	public void setEdge(@NotNull HalfEdge<P> edge) {
 		this.edge = edge;
+	}
+
+	public HalfEdge<P> getEdge() {
+		return edge;
 	}
 
 	/**
@@ -89,6 +103,19 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 		return new VPolygon(path2D);
 	}
 
+	public VTriangle toTriangle() {
+		List<HalfEdge<P>> edges = getEdges();
+		if(edges.size() != 3) {
+			throw new IllegalArgumentException("this face is not a feasible triangle.");
+		}
+		else {
+			VPoint p1 = new VPoint(edges.get(0).getEnd().getX(), edges.get(0).getEnd().getY());
+			VPoint p2 = new VPoint(edges.get(1).getEnd().getX(), edges.get(1).getEnd().getY());
+			VPoint p3 = new VPoint(edges.get(2).getEnd().getX(), edges.get(2).getEnd().getY());
+			return new VTriangle(p1, p2, p3);
+		}
+	}
+
 	@Override
 	public Iterator<HalfEdge<P>> iterator() {
 		return new HalfEdgeIterator();
@@ -101,6 +128,10 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 
 	public List<HalfEdge<P>> getEdges() {
 		return stream().collect(Collectors.toList());
+	}
+
+	public Stream<MLine<P>> streamLines() {
+		return stream().map(halfEdge -> new MLine(halfEdge.getPrevious().getEnd(), halfEdge.getEnd()));
 	}
 
 	public Stream<P> streamPoints() {
