d4a24c6d6c9425a861cd945af7b87a913246c23c
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2017-02-07 17:42:22 +0100

    implemented the FMM on uniform triangulation

--- VadereUtils/src/org/vadere/util/geometry/data/Face.java
+++ VadereUtils/src/org/vadere/util/geometry/data/Face.java
@@ -24,27 +24,45 @@ import java.util.stream.StreamSupport;
  */
 public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 
-	public static <P extends IPoint> Face<P> of(P p1, P p2, P p3) {
+	public static <P extends IPoint> Face<P> of(P x, P y, P z) {
 		Face superTriangle = new Face();
-		HalfEdge edge1 = new HalfEdge(p1, superTriangle);
-		HalfEdge edge2 = new HalfEdge(p2, superTriangle);
-		HalfEdge edge3 = new HalfEdge(p3, superTriangle);
-		edge1.setNext(edge2);
-		edge2.setNext(edge3);
-		edge3.setNext(edge1);
-		superTriangle.setEdge(edge1);
+		Face borderFace = new Face(true);
+		HalfEdge xy = new HalfEdge(y, superTriangle);
+		HalfEdge yz = new HalfEdge(z, superTriangle);
+		HalfEdge zx = new HalfEdge(x, superTriangle);
+
+		xy.setNext(yz);
+		yz.setNext(zx);
+		zx.setNext(xy);
+
+		HalfEdge yx = new HalfEdge(x, borderFace);
+		HalfEdge zy = new HalfEdge(y, borderFace);
+		HalfEdge xz = new HalfEdge(z, borderFace);
+
+		yx.setNext(xz);
+		xz.setNext(zy);
+		zy.setNext(yx);
+
+		xy.setTwin(yx);
+		yz.setTwin(zy);
+		zx.setTwin(xz);
+
+		superTriangle.setEdge(xy);
+		borderFace.setEdge(yx);
 		return superTriangle;
 	}
 
 
 	public static <P extends IPoint> Face<P> getBorder(Class<P> p) {
-		return new Face<>();
+		return new Face<>(true);
 	}
 
 	/**
 	 * One of the half-edges bordering this face.
 	 */
 	private HalfEdge<P> edge;
+
+	private boolean border;
 
 	/**
 	 * Default constructor. To construct a face where you have already some half-edges
@@ -53,6 +71,11 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 	 * @param edge one of the half-edges bordering this face.
 	 */
 	public Face(@NotNull final HalfEdge<P> edge) {
+		this(edge, false);
+	}
+
+	public Face(@NotNull final HalfEdge<P> edge, boolean border) {
+		this.border = border;
 		this.edge = edge;
 	}
 
@@ -60,10 +83,21 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 	 * This constructor can be used for constructing a new face without having
 	 * constructed the bordering half-edges jet.
 	 */
-	public Face() {}
+	public Face(boolean border) {
+		this.border = border;
+	}
+
+	public Face() {
+		this.border = false;
+	}
+
+	public boolean isBorder() {
+		return border;
+	}
 
 	/**
 	 * Sets one of the half-edges bordering this face.
+	 *
 	 * @param edge half-edge bordering this face
 	 */
 	public void setEdge(@NotNull HalfEdge<P> edge) {
@@ -76,6 +110,7 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 
 	/**
 	 * Computes the area of this face.
+	 *
 	 * @return the area of this face
 	 */
 	public double getArea() {
@@ -90,10 +125,21 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 		return streamPoints().collect(Collectors.toList());
 	}
 
+	/**
+	 * Returns true if and only if the point contained in this face.
+	 *
+	 * @param point the point which might be contained
+	 * @return true if and only if the point contained in this face.
+	 */
 	public boolean contains(final P point) {
 		return toPolygon().contains(point);
 	}
 
+	/**
+	 * Transforms this face into a Polygon object.
+	 *
+	 * @return the Polygon object defined by this face
+	 */
 	public VPolygon toPolygon() {
 		Path2D path2D = new Path2D.Double();
 		path2D.moveTo(edge.getPrevious().getEnd().getX(), edge.getPrevious().getEnd().getY());
@@ -103,6 +149,12 @@ public class Face<P extends IPoint> implements Iterable<HalfEdge<P>> {
 		return new VPolygon(path2D);
 	}
 
+	/**
+	 * Transforms this face into a triangle. Assumption: The face is a valid triangle.
+	 *
+	 * @throws IllegalArgumentException if the face does not define a valid triangle
+	 * @return a triangle which is defined by this face
+	 */
 	public VTriangle toTriangle() {
 		List<HalfEdge<P>> edges = getEdges();
 		if(edges.size() != 3) {
