3b9e87bd58fb7efe8e8bae60299c71d3555fe303
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2017-02-16 16:04:05 +0100

    before changing initial point generation of DSMesh

--- VadereUtils/src/org/vadere/util/triangulation/adaptive/MeshPoint.java
+++ VadereUtils/src/org/vadere/util/triangulation/adaptive/MeshPoint.java
@@ -28,6 +28,10 @@ public class MeshPoint extends MPoint implements PotentialPoint {
 		this(point.getX(), point.getY(), fixPoint);
 	}
 
+	public MeshPoint(final IPoint point, boolean fixPoint){
+		this(point.getX(), point.getY(), fixPoint);
+	}
+
 	public VPoint toVPoint() {
 		return new VPoint(getX(), getY());
 	}
--- VadereUtils/src/org/vadere/util/triangulation/adaptive/TestEnhancedVersion.java
+++ VadereUtils/src/org/vadere/util/triangulation/adaptive/TestEnhancedVersion.java
@@ -1,5 +1,7 @@
 package org.vadere.util.triangulation.adaptive;
 
+import org.vadere.util.geometry.data.Triangulation;
+import org.vadere.util.geometry.shapes.IPoint;
 import org.vadere.util.geometry.shapes.VRectangle;
 import org.vadere.util.geometry.shapes.VShape;
 
