a6475449c2004ad5941ede6a89f4d8149f695e47
JxDannyxJ <daniel.jadanec@gmx.de>
2017-03-27 18:05:58 +0200

    The Undo/Redo action has thrown exceptions when no more items were part of the stack. This exceptions are caught in the actionPerformed method and logged to the console on Log Level "Debug" with a simple "beep" sound for the user to notify an empty stack

--- VadereGui/src/org/vadere/gui/topographycreator/control/ActionRedo.java
+++ VadereGui/src/org/vadere/gui/topographycreator/control/ActionRedo.java
@@ -1,9 +1,14 @@
 package org.vadere.gui.topographycreator.control;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.ImageIcon;
+import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.UndoManager;
 
 /**
@@ -15,6 +20,7 @@ public class ActionRedo extends AbstractAction {
 	private static final long serialVersionUID = 4975524648404524891L;
 	private final UndoManager undoManager;
 	private final TopographyAction action;
+	private static Logger logger = LogManager.getLogger(ActionRedo.class);
 
 	public ActionRedo(final String name, final ImageIcon icon, final UndoManager undoManager,
 			final TopographyAction action) {
@@ -25,7 +31,12 @@ public class ActionRedo extends AbstractAction {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		undoManager.redo();
+		try {
+			undoManager.redo();
+		} catch (CannotRedoException e) {
+			logger.log(Priority.DEBUG, "Cannot redo! List of edits is empty!");
+			Toolkit.getDefaultToolkit().beep();
+		}
 		action.actionPerformed(arg0);
 	}
 }
