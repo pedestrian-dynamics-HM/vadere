a13642ed923b7d9c427bb272a3e7e1bb6cf3e8c5
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2017-12-12 16:52:28 +0100

    change location of the logger, add the possibilty to run a java main method during a maven phase

---
+++ VadereGui/src/org/vadere/gui/compileScripts/ClassListGenerator.java
@@ -0,0 +1,19 @@
+package org.vadere.gui.compileScripts;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * @author Benedikt Zoennchen
+ *
+ * Example: This script runs during the maven package phase.
+ */
+public class ClassListGenerator {
+
+    private static Logger log = LogManager.getLogger(ClassListGenerator.class);
+
+    public static void main(String... args) {
+
+    }
+
+}
