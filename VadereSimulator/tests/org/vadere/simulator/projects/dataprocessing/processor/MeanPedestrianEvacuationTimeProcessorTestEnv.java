4aa98b41496d6b83b6b50e6e20816bce339682f6
hm-schuhba1 <stefan.schuhbaeck@hm.edu>
2018-02-19 14:46:12 +0100

    Add MeanPedestrianEvacuationTimeProcessor

--- VadereSimulator/tests/org/vadere/simulator/projects/dataprocessing/processor/MeanPedestrianEvacuationTimeProcessorTestEnv.java
+++ VadereSimulator/tests/org/vadere/simulator/projects/dataprocessing/processor/MeanPedestrianEvacuationTimeProcessorTestEnv.java
@@ -3,7 +3,7 @@ package org.vadere.simulator.projects.dataprocessing.processor;
 import org.mockito.Mockito;
 import org.vadere.simulator.projects.dataprocessing.datakey.NoDataKey;
 import org.vadere.simulator.projects.dataprocessing.writer.VadereWriterFactory;
-import org.vadere.state.attributes.processor.AttributesEvacuationTimeProcessor;
+import org.vadere.state.attributes.processor.AttributesMeanPedestrianEvacuationTimeProcessor;
 import org.vadere.state.scenario.Pedestrian;
 
 import java.util.ArrayList;
@@ -12,28 +12,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
-import static org.mockito.Mockito.when;
-
-public class EvacuationTimeProcessorTestEnv extends ProcessorTestEnv<NoDataKey, Double> {
+public class MeanPedestrianEvacuationTimeProcessorTestEnv extends ProcessorTestEnv<NoDataKey, Double> {
 
 	private PedestrianListBuilder b = new PedestrianListBuilder();
 
-	EvacuationTimeProcessorTestEnv() {
-		this(1);
-	}
-
 	@SuppressWarnings("unchecked")
-	private EvacuationTimeProcessorTestEnv(int nextProcessorId) {
-		testedProcessor = processorFactory.createDataProcessor(EvacuationTimeProcessor.class);
-		testedProcessor.setId(nextProcessorId);
-		this.nextProcessorId = nextProcessorId + 1;
+	MeanPedestrianEvacuationTimeProcessorTestEnv() {
+		testedProcessor = processorFactory.createDataProcessor(MeanPedestrianEvacuationTimeProcessor.class);
+		testedProcessor.setId(nextProcessorId());
 
 		DataProcessor pedEvacTimeProc;
 		PedestrianEvacuationTimeProcessorTestEnv pedEvacTimeProcEnv;
 		int pedEvacTimeProcId = nextProcessorId();
 
 		//add ProcessorId of required Processors to current Processor under test
-		AttributesEvacuationTimeProcessor attr = (AttributesEvacuationTimeProcessor) testedProcessor.getAttributes();
+		AttributesMeanPedestrianEvacuationTimeProcessor attr = (AttributesMeanPedestrianEvacuationTimeProcessor) testedProcessor.getAttributes();
 		attr.setPedestrianEvacuationTimeProcessorId(pedEvacTimeProcId);
 
 		//create required Processor enviroment and add it to current Processor under test
@@ -52,76 +45,45 @@ public class EvacuationTimeProcessorTestEnv extends ProcessorTestEnv<NoDataKey, 
 
 	}
 
-	void loadSimulationStateMocksNaN() {
-		clearStates();
-		loadDefaultSimulationStateMocks();
-		removeState(3);
-		addSimState(new SimulationStateMock(4) {
-			@Override
-			public void mockIt() {
-
-				b.clear().add(new Integer[]{1});
-
-				when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
-				when(state.getSimTimeInSec()).thenReturn(40.0);
-
-				addToExpectedOutput(NoDataKey.key(), Double.NaN);
-
-			}
-		});
-	}
-
 	@Override
 	public void loadDefaultSimulationStateMocks() {
 		addSimState(new SimulationStateMock(1) {
 			@Override
 			public void mockIt() {
-
-				b.clear().add(1, 3, 4);
-
-				when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
-				when(state.getSimTimeInSec()).thenReturn(0.4);
-
+				b.clear().add(1, 2, 3);
+				Mockito.when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
+				Mockito.when(state.getSimTimeInSec()).thenReturn(0.0);
 			}
 		});
 
 		addSimState(new SimulationStateMock(2) {
 			@Override
 			public void mockIt() {
-
-				b.clear().add(1, 3, 4, 5, 8);
-
-				when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
-				when(state.getSimTimeInSec()).thenReturn(12.8);
-
+				b.clear().add(1, 2, 3, 4, 5);
+				Mockito.when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
+				Mockito.when(state.getSimTimeInSec()).thenReturn(5.0);
 			}
 		});
 
 		addSimState(new SimulationStateMock(3) {
 			@Override
 			public void mockIt() {
-
-				b.clear().add(1, 5, 8);
-
-				when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
-				when(state.getSimTimeInSec()).thenReturn(34.7);
-
-
+				b.clear().add(1, 2, 3, 4, 5, 6);
+				Mockito.when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
+				Mockito.when(state.getSimTimeInSec()).thenReturn(10.0);
 			}
 		});
-
 
 		addSimState(new SimulationStateMock(4) {
 			@Override
 			public void mockIt() {
+				b.clear().add(6);
+				Mockito.when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
+				Mockito.when(state.getSimTimeInSec()).thenReturn(15.0);
 
-				b.clear();
+				double meanEvacTime = (3 * (10.0 - 0.0) + 2 * (10.0 - 5.0)) / 5;
 
-				when(state.getTopography().getElements(Pedestrian.class)).thenReturn(b.getList());
-				when(state.getSimTimeInSec()).thenReturn(40.0);
-
-				addToExpectedOutput(NoDataKey.key(), 34.7 - 0.4);
-
+				addToExpectedOutput(NoDataKey.key(), meanEvacTime);
 			}
 		});
 	}
