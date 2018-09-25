9e8f16242b005534569484ecf636e2d14b8793fa
hm-mgoedel <marion.goedel@hm.edu>
2018-09-25 16:09:25 +0200

    Added a new processor (only for BHM!): PedestrianBehaviourProcessor

---
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/PedestrianBehaviourProcessor.java
@@ -0,0 +1,32 @@
+package org.vadere.simulator.projects.dataprocessing.processor;
+
+import org.vadere.annotation.factories.dataprocessors.DataProcessorClass;
+import org.vadere.simulator.control.SimulationState;
+import org.vadere.simulator.models.bhm.PedestrianBHM;
+import org.vadere.simulator.projects.dataprocessing.datakey.TimestepPedestrianIdKey;
+import org.vadere.state.scenario.Pedestrian;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * @author Marion Gödel
+ *
+ */
+@DataProcessorClass()
+public class PedestrianBehaviourProcessor extends DataProcessor<TimestepPedestrianIdKey, Integer> {
+
+	public PedestrianBehaviourProcessor() {
+		super("behaviourId");
+	}
+
+	@Override
+	public void doUpdate(final SimulationState state) {
+		Collection<Pedestrian> peds = state.getTopography().getElements(Pedestrian.class);
+
+		peds.stream().filter(pedestrian -> pedestrian instanceof PedestrianBHM).forEach(p -> this.putValue(new TimestepPedestrianIdKey(state.getStep(), p.getId()),
+				((PedestrianBHM) p).getBehaviour()));
+	}
+
+}
