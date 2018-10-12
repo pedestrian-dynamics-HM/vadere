9b946fab0dc70e06403d9698142086108a5c7980
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2018-10-12 13:33:24 +0200

    quick fix of circle drawing for large coordinates.

--- VadereGui/src/org/vadere/gui/renderer/agent/AgentRender.java
+++ VadereGui/src/org/vadere/gui/renderer/agent/AgentRender.java
@@ -4,11 +4,20 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.vadere.gui.components.model.SimulationModel;
+import org.vadere.gui.components.view.DefaultRenderer;
 import org.vadere.state.scenario.Pedestrian;
 import org.vadere.state.scenario.ScenarioElement;
+import org.vadere.util.geometry.GeometryUtils;
+import org.vadere.util.geometry.shapes.VCircle;
+import org.vadere.util.geometry.shapes.VPoint;
+import org.vadere.util.geometry.shapes.VPolygon;
 import org.vadere.util.geometry.shapes.VShape;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,7 +32,7 @@ public class AgentRender implements Renderer {
 	private final Color defaultColor;
 	private ConcurrentHashMap<Integer, Color> colorMap;
 
-	public AgentRender(SimulationModel model) {
+	public AgentRender(@NotNull final SimulationModel model) {
 		this.model = model;
 		this.defaultColor = model.config.getPedestrianDefaultColor();
 		this.colorMap = new ConcurrentHashMap<>();
@@ -51,12 +60,21 @@ public class AgentRender implements Renderer {
 
 	private void renderGroup(Pedestrian ped, Graphics2D g) {
 		g.setColor(getGroupColor(ped));
-		g.fill(getShape(ped));
+		DefaultRenderer.fill(getShape(ped), g);
 	}
 
 	private void renderDefault(final ScenarioElement element, Graphics2D g, Color c) {
 		g.setColor(c);
-		g.fill(element.getShape());
+		/*VCircle circle = (VCircle) element.getShape();
+		Pedestrian ped = (Pedestrian) element;
+		Ellipse2D.Float ellipse = new Ellipse2D.Float((float)ped.getPosition().x, (float)ped.getPosition().y, (float)ped.getRadius() * 2, (float)ped.getRadius() * 2);
+		Rectangle2D.Double rect = new Rectangle2D.Double(ped.getPosition().x, ped.getPosition().y, ped.getRadius() * 2, ped.getRadius() * 2);*/
+		//g.fill(new VCircle(ped.getPosition(), ped.getRadius()));
+		DefaultRenderer.fill(element.getShape(), g);
+		//g.draw(ellipse);
+		//DefaultRenderer.fill(ellipse, g);
+		//g.fill(ellipse);
+		//g.fill(rect);
 	}
 
 	private Color getHSBColor(int groupId) {
