5c2ee36667238a7eeda3b171199f32ab46354647
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2018-11-08 15:13:10 +0100

    Added class "CognitionLayer.java".

---
+++ VadereSimulator/src/org/vadere/simulator/control/cognition/CognitionLayer.java
@@ -0,0 +1,19 @@
+package org.vadere.simulator.control.cognition;
+
+import org.vadere.state.events.types.Event;
+import org.vadere.state.scenario.Pedestrian;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+/**
+ * The CognitionLayer class should provide logic to prioritize {@link org.vadere.state.events.types.Event}
+ * objects based on attributes of a pedestrian.
+ */
+public class CognitionLayer {
+
+    public void prioritizeEvents(List<Event> events, List<Pedestrian> pedestrians){
+        // TODO: Add "Event" field to class pedestrian and set it here.
+        throw new NotImplementedException();
+    }
+}
