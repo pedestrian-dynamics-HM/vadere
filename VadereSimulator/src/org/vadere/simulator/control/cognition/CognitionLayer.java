c91a7b0a15ee87a15db7ac5d813703503514ec39
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2018-11-09 09:41:16 +0100

    Added first dummy implementation of class "simulator/control/cognition/CognitionLayer".
    This new class interacts with class "Pedestrian". Therefore, added variable "mostImportantEvent" to "Pedestrian" and reformatted this class.

--- VadereSimulator/src/org/vadere/simulator/control/cognition/CognitionLayer.java
+++ VadereSimulator/src/org/vadere/simulator/control/cognition/CognitionLayer.java
@@ -1,19 +1,42 @@
 package org.vadere.simulator.control.cognition;
 
+import org.vadere.state.events.types.ElapsedTimeEvent;
 import org.vadere.state.events.types.Event;
+import org.vadere.state.events.types.WaitEvent;
 import org.vadere.state.scenario.Pedestrian;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * The CognitionLayer class should provide logic to prioritize {@link org.vadere.state.events.types.Event}
+ * The CognitionLayer class should provide logic to prioritize {@link Event}
  * objects based on attributes of a pedestrian.
  */
 public class CognitionLayer {
 
-    public void prioritizeEvents(List<Event> events, List<Pedestrian> pedestrians){
-        // TODO: Add "Event" field to class pedestrian and set it here.
-        throw new NotImplementedException();
+    public void prioritizeEventsForPedestrians(List<Event> events, List<Pedestrian> pedestrians){
+        for (Pedestrian pedestrian : pedestrians) {
+            // TODO: prioritize the events for the current time step for each pedestrian individually.
+            // by using a finite state machine, weight pedestrian's attributes or any other good mechanism.
+            Event mostImportantEvent = rankWaitHigherThanElapsedTime(events);
+            pedestrian.setMostImportantEvent(mostImportantEvent);
+        }
     }
+
+    private Event rankWaitHigherThanElapsedTime(List<Event> events) {
+        // TODO: replace dummy implementation here.
+        Event mostImportantEvent = null;
+
+        List<Event> waitEvents = events.stream().filter(event -> event instanceof WaitEvent).collect(Collectors.toList());
+
+        if (waitEvents.size() >= 1) {
+            mostImportantEvent = waitEvents.get(0);
+        } else {
+            List<Event> elapsedTimeEvents = events.stream().filter(event -> event instanceof ElapsedTimeEvent).collect(Collectors.toList());
+            mostImportantEvent = elapsedTimeEvents.get(0);
+        }
+
+        return mostImportantEvent;
+    }
+
 }
