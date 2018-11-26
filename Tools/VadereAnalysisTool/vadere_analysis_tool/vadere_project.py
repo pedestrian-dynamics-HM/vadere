5cabc4a7244a57a470c6dbf6b3d6b2679b828a12
BZoennchen <zoennchen.benedikt@hm.edu>
2018-11-26 16:47:27 +0100

    Update vadere_project.py

--- Tools/VadereAnalysisTool/vadere_analysis_tool/vadere_project.py
+++ Tools/VadereAnalysisTool/vadere_analysis_tool/vadere_project.py
@@ -116,6 +116,7 @@ class VadereProject():
         err_dir = list()
 
         out_dirs = [dir for dir in os.listdir(self.output_path) if dir != 'corrupt' and dir != 'legacy' and if not dir.startswith('.')]
+        print out_dirs
         self.output_dirs = dict()
         self.named_output = NamedOutput()
 
