ef38b9826eee9807666e82a490c0bae9713d2b41
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2019-02-24 17:58:10 +0100

    install requirements every time

--- Tools/VadereAnalysisTools/VadereAnalysisTool/setup.py
+++ Tools/VadereAnalysisTools/VadereAnalysisTool/setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt', "r") as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="vadereanalysistool",
     version="0.1.0",
@@ -11,6 +14,7 @@ setuptools.setup(
     description="Import VadereProject to ease analysis",
     long_description=long_description,
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
