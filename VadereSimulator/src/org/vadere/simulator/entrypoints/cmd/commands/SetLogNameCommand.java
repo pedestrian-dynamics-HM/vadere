ba114f2a27f767b562bc5a70dbb4fbe1485f62ce
adrianhollow <tphan@hm.edu>
2019-03-07 13:50:40 +0100

    changed SetLogNameCommand again

--- VadereSimulator/src/org/vadere/simulator/entrypoints/cmd/commands/SetLogNameCommand.java
+++ VadereSimulator/src/org/vadere/simulator/entrypoints/cmd/commands/SetLogNameCommand.java
@@ -6,10 +6,16 @@ import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.apache.logging.log4j.core.async.AsyncLoggerConfig;
+import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.config.builder.api.*;
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 import org.apache.logging.log4j.core.layout.PatternLayout;
@@ -21,65 +27,25 @@ public class SetLogNameCommand implements ArgumentAction {
     @Override
     public void run(ArgumentParser parser, Argument arg, Map<String, Object> attrs, String flag, Object value) throws ArgumentParserException {
         String filename = (String) value;
-/*        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
-
-        builder.setStatusLevel(Level.ERROR);
-        builder.setConfigurationName(filename);
-// create a rolling file appender
-        LayoutComponentBuilder layoutBuilder = builder.newLayout("PatternLayout")
-                .addAttribute("pattern", "%d [%t] %-5level: %msg%n");
-        ComponentBuilder triggeringPolicy = builder.newComponent("Policies")
-                .addComponent(builder.newComponent("SizeBasedTriggeringPolicy").addAttribute("size", "10M"));
-        AppenderComponentBuilder appenderBuilder = builder.newAppender(filename, "RollingFile")
-                .addAttribute("fileName", filename)
-                .addAttribute("filePattern", filename + "-%d{MM-dd-yy}.log.gz")
-                .add(layoutBuilder)
-                .addComponent(triggeringPolicy);
-        builder.add(appenderBuilder);
-
-// create the new logger
-        builder.add(builder.newLogger("SpecifiedFileLogger", Level.DEBUG)
-                .add(builder.newAppenderRef(filename))
-                .addAttribute("additivity", false));
-
-        builder.add(builder.newRootLogger(Level.DEBUG)
-                .add(builder.newAppenderRef(filename)));
-        LoggerContext ctx = Configurator.initialize(builder.build());*/
-        ConfigurationBuilder< BuiltConfiguration > builder = ConfigurationBuilderFactory.newConfigurationBuilder();
-
-        builder.setStatusLevel( Level.ERROR);
-        builder.setConfigurationName("RollingBuilder");
-// create a console appender
-        AppenderComponentBuilder appenderBuilder = builder.newAppender("Stdout", "CONSOLE").addAttribute("target",
-                ConsoleAppender.Target.SYSTEM_OUT);
-        appenderBuilder.add(builder.newLayout("PatternLayout")
-                .addAttribute("pattern", "%d [%t] %-5level: %msg%n%throwable"));
-        builder.add( appenderBuilder );
-// create a rolling file appender
-        LayoutComponentBuilder layoutBuilder = builder.newLayout("PatternLayout")
-                .addAttribute("pattern", "%d [%t] %-5level: %msg%n");
-        ComponentBuilder triggeringPolicy = builder.newComponent("Policies")
-                .addComponent(builder.newComponent("CronTriggeringPolicy").addAttribute("schedule", "0 0 0 * * ?"))
-                .addComponent(builder.newComponent("SizeBasedTriggeringPolicy").addAttribute("size", "100M"));
-        appenderBuilder = builder.newAppender("rolling", "RollingFile")
-                .addAttribute("fileName", "target/rolling.log")
-                .addAttribute("filePattern", "target/archive/rolling-%d{MM-dd-yy}.log.gz")
-                .add(layoutBuilder)
-                .addComponent(triggeringPolicy);
-        builder.add(appenderBuilder);
-
-// create the new logger
-        builder.add( builder.newLogger( "TestLogger", Level.DEBUG )
-                .add( builder.newAppenderRef( "rolling" ) )
-                .addAttribute( "additivity", false ) );
-
-        builder.add( builder.newRootLogger( Level.DEBUG )
-                .add( builder.newAppenderRef( "rolling" ) ) );
-        LoggerContext ctx = Configurator.initialize(builder.build());
-        // FIXME: Create a "FileAppender" which writes to "filename".
-        //   See http://logging.apache.org/log4j/2.x/manual/customconfig.html and
-        //   https://stackoverflow.com/questions/15441477/how-to-add-log4j2-appenders-at-runtime-programmatically/33472893#33472893
-        // Logger.setFileName(filename); //todo set Filename of Log-file
+        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = ctx.getConfiguration();
+        final LoggerConfig logConfig = config.getLoggerConfig("STDOUTERR");
+        final LoggerConfig rootConfig = config.getRootLogger();
+        rootConfig.removeAppender("FILE");
+        logConfig.removeAppender("FILE");
+        RollingFileAppender oldAppender = (RollingFileAppender) config.getAppender("FILE");
+        RollingFileAppender newAppender = RollingFileAppender.newBuilder()
+                .withName("FILE")
+                .withFileName(filename)
+                .withFilePattern(filename + "-%d{MM-dd-yy-HH-mm-ss}-%i.log.gz")
+                .withLayout(oldAppender.getLayout())
+                .withPolicy(oldAppender.getTriggeringPolicy())
+                .build();
+        newAppender.start();
+        config.addAppender(newAppender);
+        logConfig.addAppender(newAppender, null, null);
+        rootConfig.addAppender(newAppender, null, null);
+        ctx.updateLoggers();
     }
 
     @Override
