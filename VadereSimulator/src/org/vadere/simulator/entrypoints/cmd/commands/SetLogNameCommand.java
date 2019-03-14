ce91257fe85000720648842acf06f1b91a252fed
adrianhollow <tphan@hm.edu>
2019-03-14 10:00:06 +0100

    improved code quality

--- VadereSimulator/src/org/vadere/simulator/entrypoints/cmd/commands/SetLogNameCommand.java
+++ VadereSimulator/src/org/vadere/simulator/entrypoints/cmd/commands/SetLogNameCommand.java
@@ -4,22 +4,11 @@ import net.sourceforge.argparse4j.inf.Argument;
 import net.sourceforge.argparse4j.inf.ArgumentAction;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
-
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.appender.RollingFileAppender;
-import org.apache.logging.log4j.core.async.AsyncLoggerConfig;
 import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.apache.logging.log4j.core.config.builder.api.*;
-import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
-import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.vadere.util.logging.Logger;
 
 import java.util.Map;
 
@@ -27,8 +16,8 @@ public class SetLogNameCommand implements ArgumentAction {
     @Override
     public void run(ArgumentParser parser, Argument arg, Map<String, Object> attrs, String flag, Object value) throws ArgumentParserException {
         String filename = (String) value;
-        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        final Configuration config = ctx.getConfiguration();
+        final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = loggerContext.getConfiguration();
         final LoggerConfig logConfig = config.getLoggerConfig("STDOUTERR");
         final LoggerConfig rootConfig = config.getRootLogger();
         rootConfig.removeAppender("FILE");
@@ -45,7 +34,7 @@ public class SetLogNameCommand implements ArgumentAction {
         config.addAppender(newAppender);
         logConfig.addAppender(newAppender, null, null);
         rootConfig.addAppender(newAppender, null, null);
-        ctx.updateLoggers();
+        loggerContext.updateLoggers();
     }
 
     @Override
