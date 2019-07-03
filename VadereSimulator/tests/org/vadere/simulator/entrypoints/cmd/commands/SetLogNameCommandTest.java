0195ad5137fbce48886d42ba57cf80b55a1bc0d1
Benedikt Zoennchen <zoennchen.benedikt@hm.edu>
2019-07-03 15:46:36 +0200

    replace external scenario file by test sceanrio file.

--- VadereSimulator/tests/org/vadere/simulator/entrypoints/cmd/commands/SetLogNameCommandTest.java
+++ VadereSimulator/tests/org/vadere/simulator/entrypoints/cmd/commands/SetLogNameCommandTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.vadere.simulator.entrypoints.cmd.VadereConsole;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -13,11 +14,12 @@ public class SetLogNameCommandTest {
 
     @Test
     public void testSetLogNameCommand(){
-        String path = "test.log";
-        VadereConsole.main(new String[] {"--logname", path, "scenario-run", "-f", "../VadereModelTests/TestOSM/scenarios/basic_2_density_discrete_ca.scenario"});
         try {
+	        String path = Paths.get(getClass().getResource("/org/vadere/simulator/entrypoints/test.scenario").toURI()).toString();
+	        System.out.println(path);
+	        VadereConsole.main(new String[] {"--logname", path, "scenario-run", "-f", path});
             assertTrue(Files.lines(Paths.get(path)).count() > 0);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             fail(e.getMessage());
         }
     }
