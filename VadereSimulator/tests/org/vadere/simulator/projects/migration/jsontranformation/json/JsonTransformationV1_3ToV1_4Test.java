f632c388005538cad442bdea9216b971680d7713
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2019-09-05 15:52:03 +0200

    Updated scenario files under "TestSalientBehavior" to version 1.4

--- VadereSimulator/tests/org/vadere/simulator/projects/migration/jsontranformation/json/JsonTransformationV1_3ToV1_4Test.java
+++ VadereSimulator/tests/org/vadere/simulator/projects/migration/jsontranformation/json/JsonTransformationV1_3ToV1_4Test.java
@@ -1,7 +1,6 @@
 package org.vadere.simulator.projects.migration.jsontranformation.json;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Test;
 import org.vadere.simulator.projects.migration.MigrationException;
 import org.vadere.simulator.projects.migration.jsontranformation.JsonTransformationTest;
