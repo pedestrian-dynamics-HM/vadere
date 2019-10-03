5d1877579a84912f0172c516cbc34b2450c65d54
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2019-10-03 16:05:12 +0100

    In "SettingsDialog" fixed "Color by Target Id" (but configured color is still ignored in renderer)

--- VadereGui/src/org/vadere/gui/components/control/simulation/ActionSetPedestrianColor.java
+++ VadereGui/src/org/vadere/gui/components/control/simulation/ActionSetPedestrianColor.java
@@ -10,16 +10,20 @@ import org.vadere.gui.components.model.DefaultSimulationConfig;
 import org.vadere.gui.components.model.SimulationModel;
 
 public class ActionSetPedestrianColor extends ActionSetColor {
-	private final JComboBox<Integer> comboBox;
+    private final JComboBox<Integer> comboBox;
 
-	public ActionSetPedestrianColor(final String name, final SimulationModel<? extends DefaultSimulationConfig> model, final JPanel coloredPanel,
-	                                final JComboBox<Integer> comboBox) {
-		super(name, model, coloredPanel);
-		this.comboBox = comboBox;
-	}
+    public ActionSetPedestrianColor(final String name, final SimulationModel<? extends DefaultSimulationConfig> model, final JPanel coloredPanel,
+                                    final JComboBox<Integer> comboBox) {
+        super(name, model, coloredPanel);
+        this.comboBox = comboBox;
+    }
 
-	@Override
-	protected void saveColor(Color color) {
-		model.config.setPedestrianColor(comboBox.getSelectedIndex() + 1, color);
-	}
+    @Override
+    protected void saveColor(Color color) {
+        Integer selectedTargetId = comboBox.getItemAt(comboBox.getSelectedIndex());
+
+        if (selectedTargetId != null) {
+            model.config.setPedestrianColor(selectedTargetId, color);
+        }
+    }
 }
