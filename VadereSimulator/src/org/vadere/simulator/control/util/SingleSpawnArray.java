ecf8391f4b6cbdfddc70be7208449944c4937493
Marion Goedel <marion.goedel@hm.edu>
2019-12-03 17:13:50 +0000

    Adapted spawning for Cellular Automaton: - All agents are set on a rectangular grid (they might be touching) - If random spawning is activated, the positions on the grid are shuffled
    Therefore, the sourceAttributes are necessary in the SpawnArray (and are passed via SingleSpawnArray / GroupSpawnArray and SingleSourceController / GroupSourceController).
    
    TopographyChecker tests are changed to errors such that no simulation can be run with CA and groups.

--- VadereSimulator/src/org/vadere/simulator/control/util/SingleSpawnArray.java
+++ VadereSimulator/src/org/vadere/simulator/control/util/SingleSpawnArray.java
@@ -1,21 +1,34 @@
 package org.vadere.simulator.control.util;
 
 import org.jetbrains.annotations.NotNull;
+import org.vadere.state.attributes.scenario.AttributesSource;
 import org.vadere.util.geometry.shapes.VPoint;
 import org.vadere.util.geometry.shapes.VRectangle;
 import org.vadere.util.geometry.shapes.VShape;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.function.Function;
 
 public class SingleSpawnArray extends SpawnArray {
 
+	private boolean shuffled;
+
 	public SingleSpawnArray(VShape boundShape,
 							VRectangle spawnElementBound,
 							Function<VPoint, VShape> shapeProducer,
-							SpawnOverlapCheck testFreeSpace) {
-		super(boundShape, spawnElementBound, shapeProducer, testFreeSpace);
+							SpawnOverlapCheck testFreeSpace,
+							AttributesSource sourceAttributes) {
+		super(boundShape, spawnElementBound, shapeProducer, testFreeSpace, sourceAttributes);
+		this.shuffled = false;
+	}
+
+	public void shuffleSpawnPoints(final Random random){
+		if(!shuffled) {
+			Collections.shuffle(allowedSpawnPoints, random); // shuffle list
+		}
 	}
 
 	public Optional<VPoint> getNextPosition(@NotNull final List<VShape> blockPedestrianShapes) {
--- VadereSimulator/src/org/vadere/simulator/control/util/SpawnArray.java
+++ VadereSimulator/src/org/vadere/simulator/control/util/SpawnArray.java
@@ -1,14 +1,14 @@
 package org.vadere.simulator.control.util;
 
+import org.vadere.state.attributes.scenario.AttributesSource;
 import org.vadere.util.geometry.shapes.VPoint;
 import org.vadere.util.geometry.shapes.VRectangle;
 import org.vadere.util.geometry.shapes.VShape;
 import org.vadere.util.logging.Logger;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
+
 
 public class SpawnArray {
 	private static Logger logger = Logger.getLogger(SpawnArray.class);
@@ -31,14 +31,20 @@ public class SpawnArray {
 
 	protected int nextSpawnPoint;
 
+
 	public SpawnArray(final VShape boundShape,
 					  final VRectangle spawnElementBound,
 					  Function<VPoint, VShape> shapeProducer,
-					  SpawnOverlapCheck testFreeSpace) {
+					  SpawnOverlapCheck testFreeSpace,
+					  final AttributesSource sourceAttributes) {
 		this.bound = new VRectangle(boundShape.getBounds2D());
 		this.spawnElementBound = spawnElementBound;
 		this.shapeProducer = shapeProducer;
 		this.testFreeSpace = testFreeSpace;
+
+		if(sourceAttributes.isSpawnAtGridPositionsCA()){
+			SPAWN_BUFFER = 0;
+		}
 
 		xDim = (int) (bound.width / spawnElementBound.width);
 		yDim = (int) (bound.height / spawnElementBound.height);
@@ -62,12 +68,29 @@ public class SpawnArray {
 			eY = spawnElementBound.y + spawnElementBound.height / 2 + SPAWN_BUFFER;
 		}
 
-		firstSpawnPoint = new VPoint(bound.x + eX, bound.y + eY);
+		// cellular automaton
+		double offset_x = 0;
+		double offset_y = 0;
+		if(sourceAttributes.isSpawnAtGridPositionsCA()){
+			double grid_resolution = 0.4;
+			double boundingBoxWidth = 0.5;
+			// calculate offset of boundary of source element compared to scenario
+			double tmp_offset = (boundingBoxWidth / grid_resolution) % 1;
+			double offset_topography_bound = tmp_offset * grid_resolution;
+
+			double tmp_x = (bound.x / grid_resolution) % 1;
+			offset_x = tmp_x * grid_resolution + offset_topography_bound;
+			double tmp_y = (bound.y / grid_resolution) % 1;
+			offset_y = tmp_y * grid_resolution + offset_topography_bound;
+					// defaultModel.getGridResolution()
+		}
+
+		firstSpawnPoint = new VPoint(bound.x + eX + offset_x, bound.y + eY + offset_y);
 		validSpawnPointMapInBoundShape = new HashMap<>();
 		int validIndex = 0;
 
 		for (int i = 0; i < (xDim * yDim); i++) {
-			VPoint candidatePoint = firstSpawnPoint.add(new VPoint(2 * eX * (i % xDim), 2 * eY * (i / xDim)));
+			VPoint candidatePoint = firstSpawnPoint.add(new VPoint(2 * eX * (i % xDim) + offset_x, 2 * eY * (i / xDim) + offset_y));
 			VShape candidateShape = shapeProducer.apply(candidatePoint);
 			if (boundShape.containsShape(candidateShape)) {
 				validSpawnPointMapInBoundShape.put(i, validIndex);
@@ -75,11 +98,21 @@ public class SpawnArray {
 				validIndex++;
 			}
 		}
+		/*
+		if(sourceAttributes.isSpawnAtGridPositionsCA() && sourceAttributes.isSpawnAtRandomPositions()){
+			// choose a random selection from allowed points
+			// use random object for shuffeling! -> pass random element to shuffle
+			// todo call shuffleSpawnPoints instead (with random)
+			Collections.shuffle(allowedSpawnPoints); // shuffle list before trimming
+		}
+		*/
 		allowedSpawnPoints.trimToSize();
+
 		nextSpawnPoint = 0;
 	}
 
 	public List<VPoint> getAllowedSpawnPoints() {
 		return allowedSpawnPoints;
 	}
+
 }
