99c1f82273510a4c310917d10ea5ea6fdfb7f0ab
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2019-12-04 09:31:48 +0100

    update for run

--- VadereState/src/org/vadere/state/scenario/LinearInterpolationSpawnDistribution.java
+++ VadereState/src/org/vadere/state/scenario/LinearInterpolationSpawnDistribution.java
@@ -21,11 +21,14 @@ public class LinearInterpolationSpawnDistribution implements SpawnDistribution {
     public LinearInterpolationSpawnDistribution(RandomGenerator rng, List<Double> distributionParameters){
 
         // Most correctness is checked in LinearInterpolator
-        double[] xValues = {0., 200., 400, 600, 700, 800};
-        double[] yValues = {3., 8., 2., 10, 0, 0};
+        //double[] xValues = {0., 200., 400, 600, 700, 800};
+        //double[] yValues = {3., 8., 2., 10, 0, 0};
+
+        double[] xValues = {0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400};
+        double[] yValues = {-1., 8., -1., 8., -1.0, 8., -1, 8, -1, 8, -1, 8, -1};
 
         for(double d : yValues){
-            if(d < 0){
+            if(d < -1){
                 throw new IllegalArgumentException("No negative values are allowed for yValues. Got " + d);
             }
         }
@@ -38,12 +41,11 @@ public class LinearInterpolationSpawnDistribution implements SpawnDistribution {
         //https://www.wolframalpha.com/input/?i=normal+distribution%2C+mean%3D0%2C+sd%3D3+from+-4+to+4
         this.truncNormalDist = new TruncatedNormalDistribution(
                 this.randomGenerator, 0, 3, -4, 4, 1000);
-
     }
 
     @Override
     public int getSpawnNumber(double timeCurrentEvent){
-        int spawnNumber = (int) Math.round(this.interpolator.value(timeCurrentEvent) + this.truncNormalDist.sample());
+        int spawnNumber = (int) Math.round(this.interpolator.value(timeCurrentEvent));  // + this.truncNormalDist.sample());
         spawnNumber = Math.max(0, spawnNumber);
 
         //System.out.println("timeCurrentEvent " + timeCurrentEvent + " spawn number = " + spawnNumber);
