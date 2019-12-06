e4e783d32b59f813155546ea711de887885eaf59
schuegra <philipp.schuegraf@hm.edu>
2019-12-06 17:59:12 +0100

    [TraCI] Parameterised port of TraCIEntryPoint
    and added ArgParser to Py4jClient

--- VadereManager/src/org/vadere/manager/client/TraCIEntryPoint.java
+++ VadereManager/src/org/vadere/manager/client/TraCIEntryPoint.java
@@ -4,18 +4,33 @@
 
 package org.vadere.manager.client;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.internal.HelpScreenException;
+import org.vadere.manager.Manager;
 import org.vadere.manager.TraCISocket;
+import org.vadere.manager.VadereServer;
 import org.vadere.manager.client.traci.PersonAPI;
 import org.vadere.manager.client.traci.PolygonAPI;
 import org.vadere.manager.client.traci.SimulationAPI;
+import org.vadere.util.logging.Logger;
 import py4j.GatewayServer;
 
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TraCIEntryPoint implements Runnable {
+
+    static Logger logger;
+
     protected org.vadere.manager.client.traci.SimulationAPI simulationapi;
     protected org.vadere.manager.client.traci.PolygonAPI polygonapi;
     protected org.vadere.manager.client.traci.PersonAPI personapi;
@@ -27,17 +42,91 @@ public class TraCIEntryPoint implements Runnable {
     private String basePath = "";
     private String defaultScenario = "";
 
-    TraCIEntryPoint(int port) {
-        this.port = port;
-    }
-
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        TraCIEntryPoint entryPoint = new TraCIEntryPoint(9999);
-        //todo start server new Manager()....
-        //Manager.createArgumentParser
-        //todo parser same as Manager.addOptionsToParser
-        entryPoint.run();
+        Logger.setMainArguments(args);
+        logger = Logger.getLogger(Manager.class);
+        ArgumentParser p = createArgumentParser();
+        Namespace ns;
+
+        try {
+            ns = p.parseArgs(args);
+            ExecutorService pool = Executors.newFixedThreadPool(ns.getInt("clientNum"));
+            ServerSocket serverSocket = new ServerSocket(ns.getInt("port"));
+            logger.infof("Start Server(%s) with Loglevel: %s", VadereServer.currentVersion.getVersionString(), logger.getLevel().toString());
+            Thread serverThread = new Thread(new VadereServer(serverSocket, pool, Paths.get(ns.getString("output-dir")), ns.getBoolean("guiMode")));
+            serverThread.start();
+            TraCIEntryPoint entryPoint = new TraCIEntryPoint(ns.getInt("port"));
+            entryPoint.run();
+        } catch (HelpScreenException ignored) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArgumentParser createArgumentParser() {
+        ArgumentParser parser = ArgumentParsers.newArgumentParser("Vadere Server")
+                .defaultHelp(true)
+                .description("Runs the VADERE pedestrian simulator as a server for Python.");
+
+        addOptionsToParser(parser);
+
+        return parser;
+    }
+
+    private static void addOptionsToParser(ArgumentParser parser) {
+        // no action required call to  Logger.setMainArguments(args) already configured Logger.
+        parser.addArgument("--loglevel")
+                .required(false)
+                .type(String.class)
+                .dest("loglevel")
+                .choices("OFF", "FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE", "ALL")
+                .setDefault("INFO")
+                .help("Set Log Level.");
+
+        // no action required call to  Logger.setMainArguments(args) already configured Logger.
+        parser.addArgument("--logname")
+                .required(false)
+                .type(String.class)
+                .dest("logname")
+                .help("Write log to given file.");
+
+
+        // no action required call to  Logger.setMainArguments(args) already configured Logger.
+        parser.addArgument("--port")
+                .required(false)
+                .type(Integer.class)
+                .setDefault(9999)
+                .dest("port")
+                .help("Set port number.");
+
+        // no action required call to  Logger.setMainArguments(args) already configured Logger.
+        parser.addArgument("--clientNum")
+                .required(false)
+                .type(Integer.class)
+                .setDefault(4)
+                .dest("clientNum")
+                .help("Set number of clients to manager. Important: Each client has a separate simulation. No communication between clients");
+
+        // boolean switch to tell server to start in gui mode.
+        parser.addArgument("--gui-mode")
+                .required(false)
+                .action(Arguments.storeTrue())
+                .type(Boolean.class)
+                .dest("guiMode")
+                .help("Start server with GUI support. If a scenario is received show the current state of the scenario");
+
+        parser.addArgument("--output-dir", "-o")
+                .required(false)
+                .setDefault("./vadere-server-output")
+                .dest("output-dir") // set name in namespace
+                .type(String.class)
+                .help("Supply output directory as base directory for received scenarios.");
+    }
+
+    TraCIEntryPoint(int port) {
+        this.port = port;
     }
 
     private void init() {
