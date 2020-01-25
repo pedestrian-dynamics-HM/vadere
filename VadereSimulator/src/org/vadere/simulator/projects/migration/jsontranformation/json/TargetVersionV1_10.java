6cc1e7a2f05397510b8479972bf8a3a2221e3b7b
Benedikt Kleinmeier <benedikt.kleinmeier@hm.edu>
2020-01-25 08:47:20 +0100

    Added "TargetVersionV1_10" to remove "threatMemory" node

---
+++ VadereSimulator/src/org/vadere/simulator/projects/migration/jsontranformation/json/TargetVersionV1_10.java
@@ -0,0 +1,41 @@
+package org.vadere.simulator.projects.migration.jsontranformation.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.vadere.annotation.factories.migrationassistant.MigrationTransformation;
+import org.vadere.simulator.entrypoints.Version;
+import org.vadere.simulator.projects.migration.MigrationException;
+import org.vadere.simulator.projects.migration.jsontranformation.SimpleJsonTransformation;
+
+/**
+ * Remove node "threatMemory" under "scenario.topography.dynamicElements.psychologyStatus"
+ */
+@MigrationTransformation(targetVersionLabel = "1.10")
+public class TargetVersionV1_10 extends SimpleJsonTransformation {
+
+	public TargetVersionV1_10(){
+		super(Version.V1_10);
+	}
+
+	@Override
+	protected void initDefaultHooks() {
+		addPostHookFirst(this::removeThreatMemoryNode);
+		addPostHookLast(this::sort);
+	}
+
+	public JsonNode removeThreatMemoryNode(JsonNode node) throws MigrationException {
+		String nodeNameToRemove = "threatMemory";
+
+		JsonNode dynamicElementsNode = path(node, "scenario/topography/dynamicElements");
+
+		if (dynamicElementsNode.isArray()) {
+			for (JsonNode dynamicElementNode : dynamicElementsNode) {
+				JsonNode psychologyStatusNode = path(dynamicElementNode, "psychologyStatus");
+
+				remove(psychologyStatusNode, nodeNameToRemove);
+			}
+		}
+
+		return node;
+	}
+
+}
