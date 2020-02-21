05d44359421349da7f4a90e05d677290d0c7f4e1
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2020-02-21 23:28:48 +0100

    add histogram

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/AreaDensityHistProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/AreaDensityHistProcessor.java
@@ -1,7 +1,5 @@
 package org.vadere.simulator.projects.dataprocessing.processor;
 
-import org.apache.commons.math.random.EmpiricalDistributionImpl;
-import org.apache.commons.math3.stat.Frequency;
 import org.vadere.annotation.factories.dataprocessors.DataProcessorClass;
 import org.vadere.simulator.control.simulation.SimulationState;
 import org.vadere.simulator.projects.dataprocessing.datakey.TimestepKey;
@@ -10,16 +8,24 @@ import org.vadere.state.attributes.processor.AttributesProcessor;
 import org.vadere.state.scenario.DynamicElementContainer;
 import org.vadere.state.scenario.Pedestrian;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Daniel Lehmberg
  */
 
 @DataProcessorClass(label = "AreaDensityHistProcessor")
-public class AreaDensityHistProcessor extends DataProcessor<TimestepKey, List<Double>>  {
+public class AreaDensityHistProcessor extends DataProcessor<TimestepKey, ArrayList<Integer>>  {
+
+    static class IntegerArrayListCSV extends ArrayList<Integer>{
+        @Override
+        public String toString() {
+            String row = "";
+            for(Integer i : this){}
+            return row;
+        }
+    }
 
     public AreaDensityHistProcessor() {
         super();
@@ -31,10 +37,34 @@ public class AreaDensityHistProcessor extends DataProcessor<TimestepKey, List<Do
         return (AttributesAreaDensityHistProcessor) this.getAttributes();
     }
 
+    private ArrayList<Integer> computeHistogram(double[] data, double min, double max) {
+        int nBins = this.getCastedAttributes().getNrBins();
+
+        // pre-allocate:
+        ArrayList<Integer> result = new ArrayList<>(nBins);
+
+        // set all values to zero
+        for(int i = 0; i < nBins; ++i){
+            result.add(0);
+        }
+
+        final double binSize = (max - min) / nBins;
+
+        for (double d : data) {
+            int bin = (int) ((d - min) / binSize);
+            if (bin < 0) { /* this data is smaller than min */ }
+            else if (bin >= nBins) { /* this data point is bigger than max */ }
+            else {
+                result.set(bin, result.get(bin) + 1);
+            }
+        }
+        return result;
+    }
+
+
+    @Override
     public void preLoop(final SimulationState state) {
-
         // TODO: make checks here!
-
         int nrBins = this.getCastedAttributes().getNrBins();
 
         // Headers can only be set according to the user setting nrBins
@@ -56,10 +86,8 @@ public class AreaDensityHistProcessor extends DataProcessor<TimestepKey, List<Do
         Collection<Pedestrian> pedCollection = pedestrians.getElements();
 
         double[] directionPosition = pedCollection.stream().mapToDouble(ped -> ped.getPosition().x).toArray();
-        EmpiricalDistributionImpl histogram = new EmpiricalDistributionImpl(getCastedAttributes().getNrBins());
-        histogram.load(directionPosition);
 
-        List<Double> binValues = histogram.getBinStats().stream().map(binStat -> binStat.getSum()).collect(Collectors.toList());
+        ArrayList<Integer> binValues = this.computeHistogram(directionPosition, 0, state.getTopography().getBounds().width);
         this.putValue(new TimestepKey(state.getStep()), binValues);
     }
 
