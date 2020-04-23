83bc6dd07969eb83a15653ed40212f20fe089bc4
Christina <mayr9@hm.edu>
2020-04-23 13:18:46 +0200

    Extended data processor with InformationDegree

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/PedStimulusCountingProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/PedStimulusCountingProcessor.java
@@ -3,24 +3,25 @@ package org.vadere.simulator.projects.dataprocessing.processor;
 import org.vadere.annotation.factories.dataprocessors.DataProcessorClass;
 import org.vadere.simulator.control.simulation.SimulationState;
 import org.vadere.simulator.projects.dataprocessing.ProcessorManager;
+import org.vadere.simulator.projects.dataprocessing.datakey.EventtimePedestrianIdKey;
 import org.vadere.simulator.projects.dataprocessing.datakey.TimestepKey;
-import org.vadere.simulator.utils.scenariochecker.checks.dataProcessors.CheckAreasInAreaDensityVoronoiProcessor;
 import org.vadere.state.attributes.processor.AttributesPedStimulusCountingProcessor;
 import org.vadere.state.scenario.Pedestrian;
+import org.vadere.state.simulation.InformationDegree;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 @DataProcessorClass(label = "PedStimulusCountingProcessor")
-public class PedStimulusCountingProcessor extends DataProcessor<TimestepKey, Double> {
+public class PedStimulusCountingProcessor extends DataProcessor<TimestepKey, InformationDegree> {
 
 	private Predicate<Pedestrian> filter_by_stimuli;
 	private Pattern filter_pattern = null;
 
-	public PedStimulusCountingProcessor(){
-		super("PercentageInformed");
-		setAttributes(new AttributesPedStimulusCountingProcessor());
+	public PedStimulusCountingProcessor() {
+		super("numberPedsInformed", "numberPedsAll", "percentageInformed");
 	}
 
 	@Override
@@ -41,10 +42,13 @@ public class PedStimulusCountingProcessor extends DataProcessor<TimestepKey, Dou
 	protected void doUpdate(SimulationState state) {
 		Collection<Pedestrian> peds = state.getTopography().getElements(Pedestrian.class);
 
-		double percentageInformed = peds.stream().filter(p -> filter_by_stimuli.test(p)).count() / peds.size();
+		int numberPedsInformed = (int) peds.stream().filter(p -> filter_by_stimuli.test(p)).count();
+		int numberPedsAll = (int) peds.stream().filter(p-> p.getFootstepHistory().getFootSteps().size() > 1).count();
 
+		numberPedsAll = Math.max(numberPedsAll,numberPedsInformed);
 
-		this.putValue(new TimestepKey(state.getStep()), percentageInformed);
+		InformationDegree informationDegree =  new InformationDegree(numberPedsInformed, numberPedsAll);
+		putValue(new TimestepKey(state.getStep()), informationDegree);
 
 	}
 
@@ -55,6 +59,14 @@ public class PedStimulusCountingProcessor extends DataProcessor<TimestepKey, Dou
 		}
 		return (AttributesPedStimulusCountingProcessor)super.getAttributes();
 	}
+
+
+	@Override
+	public String[] toStrings(TimestepKey key) {
+		String[] informationDegrees = this.getValue(key).getValueString();
+		return informationDegrees;
+	}
+
 
 
 }
