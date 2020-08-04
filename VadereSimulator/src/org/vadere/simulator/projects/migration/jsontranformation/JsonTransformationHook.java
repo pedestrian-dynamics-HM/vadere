4e2ae81aa367bdf5dd8edffcad83e426569d0037
Christina <mayr9@hm.edu>
2020-08-04 15:47:53 +0200

    removed white spaced and unnecessary import

--- VadereSimulator/src/org/vadere/simulator/projects/migration/jsontranformation/JsonTransformationHook.java
+++ VadereSimulator/src/org/vadere/simulator/projects/migration/jsontranformation/JsonTransformationHook.java
@@ -1,6 +1,5 @@
 package org.vadere.simulator.projects.migration.jsontranformation;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import org.vadere.simulator.projects.migration.MigrationException;
