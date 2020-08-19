0a16c8723f20aae5faf7b18bbb2b5a7055faefd1
Christina <mayr9@hm.edu>
2020-08-19 09:58:22 +0200

    allow user to switch display option

--- VadereState/src/org/vadere/state/attributes/processor/AttributesMeshProcessor.java
+++ VadereState/src/org/vadere/state/attributes/processor/AttributesMeshProcessor.java
@@ -3,6 +3,7 @@ package org.vadere.state.attributes.processor;
 public class AttributesMeshProcessor extends AttributesProcessor {
 	private int measurementAreaId = -1;
 	private double edgeLength = 1.0;
+	private boolean displayMesh = true;
 
 	public int getMeasurementAreaId() {
 		return this.measurementAreaId;
@@ -20,5 +21,14 @@ public class AttributesMeshProcessor extends AttributesProcessor {
 	public void setEdgeLength(final double edgeLength) {
 		checkSealed();
 		this.edgeLength = edgeLength;
+	}
+
+	public boolean isDisplayMesh() {
+		return displayMesh;
+	}
+
+	public void setDisplayMesh(boolean displayMesh) {
+		checkSealed();
+		this.displayMesh = displayMesh;
 	}
 }
