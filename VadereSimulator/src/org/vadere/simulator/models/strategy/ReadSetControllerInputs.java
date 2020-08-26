b8b9ceb7ab39431f17008575bf823676bcbe2199
Christina <mayr9@hm.edu>
2020-08-26 18:29:07 +0200

    use strategyLayer

---
+++ VadereSimulator/src/org/vadere/simulator/models/strategy/ReadSetControllerInputs.java
@@ -0,0 +1,77 @@
+package org.vadere.simulator.models.strategy;
+
+import com.github.cschen1205.fuzzylogic.Clause;
+import com.github.cschen1205.fuzzylogic.FuzzySet;
+import com.github.cschen1205.fuzzylogic.Rule;
+import com.github.cschen1205.fuzzylogic.RuleInferenceEngine;
+import com.github.cschen1205.fuzzylogic.memberships.FuzzyGrade;
+import com.github.cschen1205.fuzzylogic.memberships.FuzzyReverseGrade;
+import com.github.cschen1205.fuzzylogic.memberships.FuzzyTriangle;
+import org.vadere.simulator.control.strategy.models.navigation.INavigationModel;
+import org.vadere.simulator.projects.dataprocessing.ProcessorManager;
+import org.vadere.simulator.projects.dataprocessing.processor.AreaDensityCountingProcessor;
+import org.vadere.state.scenario.Pedestrian;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+// https://github.com/cschen1205/java-fuzzy-logic
+
+
+public class ReadSetControllerInputs implements INavigationModel {
+
+    private double[][] controllerInputs;
+    private int counter = 0;
+
+    @Override
+    public void initialize(double simTimeInSec) {
+
+        String fileName = "Scenarios/Demos/Density_controller/scenarios/TwoCorridors_forced_controller_input.csv";
+
+        try {
+            this.controllerInputs = Files.lines(Paths.get(fileName)).map(s -> s.split(" ")).map(s -> Arrays.stream(s).mapToDouble(Double::parseDouble).toArray()).toArray(double[][]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println( this.controllerInputs[0][0] );
+
+    }
+
+
+
+    public void update(double simTimeInSec, Collection<Pedestrian> pedestrians, ProcessorManager processorManager) {
+
+
+
+        double percentageLeft = controllerInputs[counter][1];
+
+        List<Pedestrian> newAgents = pedestrians.stream().filter(p -> p.getFootstepHistory().getFootSteps().size() == 0).collect(Collectors.toList());
+
+        int numberLeft = (int) (newAgents.size()*percentageLeft);
+        int numberRight = newAgents.size() - numberLeft;
+
+        LinkedList<Integer> targets = new LinkedList<Integer>();
+        for (int i = 0; i < numberLeft; i++) {
+            targets.add(2001);
+        }
+        for (int i = 0; i < numberRight; i++) {
+            targets.add(2002);
+        }
+
+        int c = 0;
+        for (Pedestrian pedestrian : newAgents) {
+
+            LinkedList<Integer> nextTargets = new LinkedList<Integer>();
+            nextTargets.add(targets.get(c));
+            nextTargets.add(1);
+            pedestrian.setTargets(nextTargets);
+            c+=1;
+        }
+
+        counter += 1;
+
+    }
+}
