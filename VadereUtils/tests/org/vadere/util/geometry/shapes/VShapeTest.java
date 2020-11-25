286ca02211cafa9408fd2c1340d3c2a5290ea5e1
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2020-11-25 16:18:34 +0000

    fix unstable VRectangle / VCircle contains calculation.

--- VadereUtils/tests/org/vadere/util/geometry/shapes/VShapeTest.java
+++ VadereUtils/tests/org/vadere/util/geometry/shapes/VShapeTest.java
@@ -1,8 +1,7 @@
 package org.vadere.util.geometry.shapes;
 
+import org.junit.Ignore;
 import org.junit.Test;
-
-import java.awt.geom.Path2D;
 
 import static org.junit.Assert.*;
 
@@ -14,6 +13,66 @@ public class VShapeTest {
 		VShape b = new VRectangle(0, 0, 1, 1);
 
 		assertTrue(a.intersects(b));
+	}
+
+	@Test(timeout = 1000) //ms
+	public void loopAtEdgeCase1(){
+		VRectangle rec = new VRectangle(0.0, 114.0, 44.0, 5.0);
+		VCircle circle = new VCircle(new VPoint(0.005096532915902063, 118.69364126200188), 0.2);
+		boolean contains = rec.containsShape(circle);
+		assertFalse(contains);
+	}
+
+	@Test(timeout = 1000) //ms
+	public void loopAtEdgeCase2(){
+		VRectangle rec = new VRectangle(0.0, 114.0, 44.0, 5.0);
+		VCircle circle = new VCircle(new VPoint(0.008096532915902063, 118.69364126200188), 0.2);
+		boolean contains = rec.containsShape(circle);
+		assertFalse(contains);
+	}
+
+	@Ignore
+	@Test(timeout = 1000) //ms
+	/**
+	 * Error at magic number. With given Shapes in this test the AWT Area equals comparision
+	 * will start loop forever. Small changes (0.005 --> 0.008) will fix this.
+	 * Problem occurs if the contains check involves circles because they get transformed
+	 * to awt Paths for the comparision (bezier curve) which is probably the reason.
+	 * Current fix introduces a new contains(VCircle otherShape) overload in which
+	 * specialized check should take place. Currently only VRectangle implement this.
+	 * Other shapes will still use the numerical unstable version.
+	 */
+	public void loopAtEdgeCase_asVPolygon1(){
+		VRectangle rec = new VRectangle(0.0, 114.0, 44.0, 5.0);
+		VCircle circle = new VCircle(new VPoint(0.005096532915902063, 118.69364126200188), 0.2);
+		VPolygon poly = new VPolygon(rec);
+		boolean contains = poly.contains(circle);
+		assertFalse(contains);
+	}
+
+	@Test(timeout = 1000)
+	public void loopAtEdgeCase_asVPolygon2(){
+		VRectangle rec = new VRectangle(0.0, 114.0, 44.0, 5.0);
+		VCircle circle = new VCircle(new VPoint(0.008096532915902063, 118.69364126200188), 0.2);
+		VPolygon poly = new VPolygon(rec);
+		boolean contains = poly.contains(circle);
+		assertFalse(contains);
+	}
+
+	@Test(timeout = 1000) //ms
+	public void loopAtEdgeCase_asVRectangle(){
+		VRectangle rec = new VRectangle(0.0, 114.0, 44.0, 5.0);
+		VCircle circle = new VCircle(new VPoint(0.005096532915902063, 118.69364126200188), 0.2);
+		boolean contains = rec.contains(circle);
+		assertFalse(contains);
+	}
+
+	@Test
+	public void containsShapeFalse(){
+		VRectangle rec = new VRectangle(0.0, 114.0, 44.0, 5.0);
+		VCircle circle = new VCircle(new VPoint(300.0, 300.0), 0.2);
+		boolean contains = rec.containsShape(circle);
+		assertFalse(contains);
 	}
 
 	@Test
