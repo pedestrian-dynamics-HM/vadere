94c01874157280b2c597d1d75b4d78105368f63e
Stefan Schuhbaeck <stefan.schuhbaeck@hm.edu>
2020-12-10 15:50:32 +0100

    More verbose traci error

--- VadereManager/src/org/vadere/manager/traci/commandHandler/variables/ControlVar.java
+++ VadereManager/src/org/vadere/manager/traci/commandHandler/variables/ControlVar.java
@@ -23,9 +23,16 @@ public enum ControlVar {
 		throw new TraCIException(String.format("No ControlVar var found with id: %02X", id));
 	}
 
+	public static String asString(int id){
+		try {
+			return fromId(id).toString();
+		} catch (TraCIException ignored) { }
+		return "No variable for id: " + id;
+	}
+
 	@Override
 	public String toString() {
-		return "ControlVar{" +
+		return name() + "{" +
 				"id=" + id +
 				", type=" + returnType +
 				'}';
