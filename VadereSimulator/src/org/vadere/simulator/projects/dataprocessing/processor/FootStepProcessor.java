708e7a94d9bad355d940d17b8d34927be44bd4ef
Christina <mayr9@hm.edu>
2021-07-05 11:14:32 +0200

    at simulation time 0: add dummy step for selfcat visualization

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/FootStepProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/FootStepProcessor.java
@@ -4,6 +4,7 @@ import org.vadere.annotation.factories.dataprocessors.DataProcessorClass;
 import org.vadere.simulator.control.simulation.SimulationState;
 import org.vadere.simulator.projects.dataprocessing.ProcessorManager;
 import org.vadere.simulator.projects.dataprocessing.datakey.EventtimePedestrianIdKey;
+import org.vadere.state.psychology.cognition.SelfCategory;
 import org.vadere.state.scenario.Pedestrian;
 import org.vadere.state.simulation.FootStep;
 import org.vadere.state.simulation.VTrajectory;
@@ -29,32 +30,67 @@ import java.util.LinkedList;
 @DataProcessorClass()
 public class FootStepProcessor extends DataProcessor<EventtimePedestrianIdKey, FootStep> {
 
-	public FootStepProcessor() {
-		super("endTime", "startX", "startY", "endX", "endY");
-	}
+    public FootStepProcessor() {
+        super("endTime", "startX", "startY", "endX", "endY");
+    }
 
-	@Override
-	protected void doUpdate(final SimulationState state) {
-		for (Pedestrian pedestrian : state.getTopography().getElements(Pedestrian.class)) {
-			LinkedList<FootStep> footSteps = pedestrian.getTrajectory().clone().getFootSteps();
+    @Override
+    protected void doUpdate(final SimulationState state) {
+        for (Pedestrian pedestrian : state.getTopography().getElements(Pedestrian.class)) {
+            LinkedList<FootStep> footSteps = pedestrian.getTrajectory().clone().getFootSteps();
 
-			for (FootStep fs : footSteps) {
-				putValue(new EventtimePedestrianIdKey(fs.getStartTime(), pedestrian.getId()), fs);
-			}
-		}
-	}
+            for (FootStep fs : footSteps) {
+                putValue(new EventtimePedestrianIdKey(fs.getStartTime(), pedestrian.getId()), fs);
+            }
 
-	@Override
-	public void init(final ProcessorManager manager) {
-		super.init(manager);
-	}
+            //TODO: refactor
+            if ((footSteps.size() == 0) && (state.getStep() == 1)
+                    && ((pedestrian.getSelfCategory() == SelfCategory.WAIT) || (pedestrian.getSelfCategory() == SelfCategory.CHANGE_TARGET))) {
+                // add dummy footstep for self category visualization
+                FootStep fs = new FootStep(pedestrian.getPosition(), pedestrian.getPosition(), 0.0, -1); // -1 needs to be corrected in the postloop
+                putValue(new EventtimePedestrianIdKey(state.getSimTimeInSec(), pedestrian.getId()), fs);
+            }
 
-	@Override
-	public String[] toStrings(EventtimePedestrianIdKey key) {
-		String[] footStepLine = this.getValue(key).getValueString();
 
-		// Note: remove the "startTime" from the footStepLine because it is already included in the "simTime" of the
-		// EventtimePedestrianIdKey
-		return Arrays.copyOfRange(footStepLine, 1, footStepLine.length);
-	}
+        }
+    }
+
+    public void postLoop(final SimulationState state) {
+
+        // add correct end time to dummy footstep
+        //TODO: refactor
+        for (EventtimePedestrianIdKey key : this.getData().keySet()){
+            FootStep firstFootstep = this.getValue(key);
+            if (firstFootstep.getEndTime() == -1) {
+                for (EventtimePedestrianIdKey nextFootstep : this.getData().keySet()) {
+                    if (nextFootstep.getPedestrianId().equals(key.getPedestrianId()) &&
+                            this.getValue(nextFootstep).getStartTime() > 0.0) {
+                        FootStep fs = new FootStep(firstFootstep.getStart(),
+                                firstFootstep.getEnd(),
+                                firstFootstep.getStartTime(),
+                                this.getValue(nextFootstep).getStartTime());
+                        this.getData().replace(key, fs);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    @Override
+    public void init(final ProcessorManager manager) {
+        super.init(manager);
+    }
+
+    @Override
+    public String[] toStrings(EventtimePedestrianIdKey key) {
+
+        String[] footStepLine = this.getValue(key).getValueString();
+
+        // Note: remove the "startTime" from the footStepLine because it is already included in the "simTime" of the
+        // EventtimePedestrianIdKey
+        return Arrays.copyOfRange(footStepLine, 1, footStepLine.length);
+    }
 }
--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/FootStepTargetIDProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/FootStepTargetIDProcessor.java
@@ -4,10 +4,13 @@ import org.vadere.annotation.factories.dataprocessors.DataProcessorClass;
 import org.vadere.simulator.control.simulation.SimulationState;
 import org.vadere.simulator.projects.dataprocessing.datakey.EventtimePedestrianIdKey;
 import org.vadere.simulator.projects.dataprocessing.processor.util.ModelFilter;
+import org.vadere.state.psychology.cognition.SelfCategory;
 import org.vadere.state.scenario.Pedestrian;
+import org.vadere.state.simulation.FootStep;
 import org.vadere.util.logging.Logger;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 @DataProcessorClass()
 public class FootStepTargetIDProcessor extends DataProcessor<EventtimePedestrianIdKey, Integer> implements ModelFilter {
@@ -19,9 +22,29 @@ public class FootStepTargetIDProcessor extends DataProcessor<EventtimePedestrian
 
 	@Override
 	protected void doUpdate(SimulationState state) {
-		Collection<Pedestrian> peds = state.getTopography().getElements(Pedestrian.class);
-		peds.forEach(p -> p.getTrajectory().getFootSteps().forEach(fs -> {
-					this.putValue(new EventtimePedestrianIdKey(fs.getStartTime(), p.getId()), !p.hasNextTarget() ? -1 : p.getNextTargetId());
-		}));
+
+		for (Pedestrian p : state.getTopography().getElements(Pedestrian.class)) {
+			LinkedList<FootStep> footSteps = p.getTrajectory().clone().getFootSteps();
+			SelfCategory selfCat = p.getSelfCategory();
+
+			for(FootStep fs : footSteps){
+				this.putValue(new EventtimePedestrianIdKey(fs.getStartTime(), p.getId()), !p.hasNextTarget() ? -1 : p.getNextTargetId());
+			}
+
+			/* If the actions WAIT and CHANGE_TARGET occur at the beginning, a dummy step is genereated in the FootStepProcessor.class
+			 * Add the target id to the dummy footstep
+			 * */
+			if ((selfCat == SelfCategory.WAIT) || (selfCat == SelfCategory.CHANGE_TARGET)) {
+				if (footSteps.size() == 0) {
+					if (state.getStep() == 1) {
+						this.putValue(new EventtimePedestrianIdKey(state.getSimTimeInSec(), p.getId()), !p.hasNextTarget() ? -1 : p.getNextTargetId());
+					}
+				}
+			}
+
+		}
+
+
+
 	}
 }
