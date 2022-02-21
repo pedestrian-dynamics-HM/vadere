a2802cc74730bec52ece4b627bbb1fc6a406179f
Simon Rahn <simon.rahn@hm.edu>
2022-02-21 10:04:58 +0100

    [wip] Remove unused methods related to deprecated healthStatus
    Rename PedestrianPathogenLoadProcessor

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/PedestrianDegreeOfExposureProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/PedestrianDegreeOfExposureProcessor.java
@@ -8,17 +8,17 @@ import org.vadere.state.scenario.Pedestrian;
 import java.util.Collection;
 
 /**
- * This processor returns the absorbedPathogenLoad per pedestrian id at each simulation step
+ * This processor returns the degree of exposure per pedestrian id at each simulation step
  */
 @DataProcessorClass()
-public class PedestrianPathogenLoadProcessor extends DataProcessor<TimestepPedestrianIdKey, Double> {
-    public PedestrianPathogenLoadProcessor() {
+public class PedestrianDegreeOfExposureProcessor extends DataProcessor<TimestepPedestrianIdKey, Double> {
+    public PedestrianDegreeOfExposureProcessor() {
         super("absorbedPathogenLoad");
     }
 
     @Override
     public void doUpdate(final SimulationState state) {
         Collection<Pedestrian> peds = state.getTopography().getElements(Pedestrian.class);
-        peds.forEach(p -> this.putValue(new TimestepPedestrianIdKey(state.getStep(), p.getId()), p.getPathogenAbsorbedLoad()));
+        peds.forEach(p -> this.putValue(new TimestepPedestrianIdKey(state.getStep(), p.getId()), p.getDegreeOfExposure()));
     }
 }
