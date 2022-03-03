3f3ee220e1af8f15090e99a05468f55af50cc810
Simon Rahn <simon.rahn@hm.edu>
2022-03-03 16:23:06 +0100

    Refactoring: In AerosolCloudDataProcessor, correct javadoc and add number format for data output

--- VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/AerosolCloudDataProcessor.java
+++ VadereSimulator/src/org/vadere/simulator/projects/dataprocessing/processor/AerosolCloudDataProcessor.java
@@ -8,20 +8,20 @@ import org.vadere.state.attributes.processor.AttributesAerosolCloudShapeProcesso
 import org.vadere.state.scenario.AerosolCloud;
 
 import java.util.Collection;
+import java.util.Locale;
 
 /**
- * <p>This processor writes out the shape and the pathogen load of all {@link AerosolCloud}s. Each row corresponds to
- * one aerosol cloud at a given simulation time. The shape of an aerosol cloud is described by the vertices
- * ([x, y]-coordinates) and the area.
+ * <p>This processor writes out the shape and the pathogen load of all {@link AerosolCloud}s.
+ * Each row corresponds to one aerosol cloud at a given simulation time. The shape of an
+ * aerosol cloud is described by the radius and its center ([x, y]-coordinates).
  * </p>
  *
- * <p>To reduce the size of the output file, one can reduce the sample frequency, i.e. the processor writes out only
- * every {@param sampleEveryNthSimStep}.
+ * <p>To reduce the size of the output file, one can reduce the sample frequency, that is
+ * the processor writes out only every {@param sampleEveryNthSimStep}.
  * </p>
  *
  * @author Simon Rahn
  */
-
 @DataProcessorClass()
 public class AerosolCloudDataProcessor extends DataProcessor<TimestepIdDataKey, String> {
 
@@ -52,7 +52,7 @@ public class AerosolCloudDataProcessor extends DataProcessor<TimestepIdDataKey, 
     }
 
     private String dataToString(AerosolCloud aerosolCloud) {
-        String dataAsString = String.format("%f %f %f %f",
+        String dataAsString = String.format(Locale.US, "%f %f %f %f",
                 aerosolCloud.getCurrentPathogenLoad(),
                 aerosolCloud.getRadius(),
                 aerosolCloud.getCenter().x,
