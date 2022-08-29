60f28c7039eb867f58fe4acefe76542bb998dd3a
Ludwig Jäck <ludwig.jaeck@hm.edu>
2022-08-29 17:13:36 +0200

    added comments to distribution parameters

--- VadereState/src/org/vadere/state/scenario/distribution/parameter/ConstantParameter.java
+++ VadereState/src/org/vadere/state/scenario/distribution/parameter/ConstantParameter.java
@@ -1,10 +1,13 @@
 package org.vadere.state.scenario.distribution.parameter;
 
 /**
- * @author Lukas Gradl (lgradl@hm.edu), Aleksandar Ivanov
+ * This is the parameter structure used with a constant distribution.
+ * @author Lukas Gradl (lgradl@hm.edu), Aleksandar Ivanov, Ludwig Jaeck
  */
-
 public class ConstantParameter {
+	/**
+	 * The attribute updateFrequency describes the time it takes for the next event to happen.
+	 */
 	double updateFrequency;
 
 	public void setUpdateFrequency(double updateFrequency){
