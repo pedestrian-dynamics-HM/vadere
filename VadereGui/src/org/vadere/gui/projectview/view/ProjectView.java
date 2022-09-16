1764b8bb0ffe569b31b5b748a50608d712157b1f
Ludwig Jäck <ludwig.jaeck@hm.edu>
2022-09-16 09:15:41 +0200

    Revert "changed LAF to FlatLaf"
    This reverts commit 2952b2589c39ccbd8e723bc5e1d5b39e30c9deb8.

--- VadereGui/src/org/vadere/gui/projectview/view/ProjectView.java
+++ VadereGui/src/org/vadere/gui/projectview/view/ProjectView.java
@@ -1,7 +1,6 @@
 package org.vadere.gui.projectview.view;
 
 
-import com.formdev.flatlaf.FlatDarkLaf;
 import org.jetbrains.annotations.NotNull;
 import org.vadere.gui.components.utils.Messages;
 import org.vadere.gui.postvisualization.control.Player;
@@ -23,18 +22,23 @@ import org.vadere.util.io.IOUtils;
 import org.vadere.util.logging.Logger;
 import org.vadere.util.opencl.CLUtils;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.*;
+import java.util.List;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
 
 /**
  * Main view of the Vadere GUI.
@@ -45,7 +49,7 @@ public class ProjectView extends JFrame implements ProjectFinishedListener, Sing
 	 * Static variables
 	 */
 	private static final long serialVersionUID = -2081363246241235943L;
-	private static final Logger logger = Logger.getLogger(ProjectView.class);
+	private static Logger logger = Logger.getLogger(ProjectView.class);
 	/**
 	 * Store a reference to the main window as "owner" parameter for dialogs.
 	 */
@@ -54,22 +58,22 @@ public class ProjectView extends JFrame implements ProjectFinishedListener, Sing
 	/**
 	 * The model of the {@link ProjectView}
 	 */
-	private final ProjectViewModel model;
+	private ProjectViewModel model;
 
 	private final int n_repetitions = 10;
 
 	/**
 	 * GUI elements (part of the view) of the {@link ProjectView}
-	 * <p>
+	 *
 	 * TODO [priority=medium] [task=refactoring] do the actions have to be stored in member
 	 * variables or could it be better to store them locally where they are needed? Some are used in
 	 * different methods, maybe only store these as members?
 	 */
-	private final JPanel contentPane = new JPanel();
-	private final JPanel controlPanel = new JPanel();
+	private JPanel contentPane = new JPanel();
+	private JPanel controlPanel = new JPanel();
 	private JSplitPane mainSplitPanel = new JSplitPane();
-	private final ProgressPanel progressPanel = new ProgressPanel();
-	private final Set<Action> projectSpecificActions = new HashSet<>(); // actions that should only be enabled, when a project is loaded
+	private VTable scenarioTable;
+	private VTable outputTable;
 	private JButton btnRunSelectedScenario;
 	private JButton btnRunRepeatedlyScenario;
 	private JButton btnRunAllScenarios;
@@ -78,12 +82,12 @@ public class ProjectView extends JFrame implements ProjectFinishedListener, Sing
 	private JButton btnNextSimulationStep;
 	private JButton btnResumeNormalSpeed;
 	private JMenu mntmRecentProjects;
-	private final ProjectRunResultDialog projectRunResultDialog;
+	private ProgressPanel progressPanel = new ProgressPanel();
 	private ScenarioPanel scenarioJPanel;
 	private ScenarioNamePanel scenarioNamePanel;
 	private boolean scenariosRunning = false;
-	private VTable<?> scenarioTable;
-	private VTable<?> outputTable;
+	private Set<Action> projectSpecificActions = new HashSet<>(); // actions that should only be enabled, when a project is loaded
+	private ProjectRunResultDialog projectRunResultDialog;
 
 	// ####################### Part of the control this should also be part of another class
 	// ##################
@@ -154,27 +158,13 @@ public class ProjectView extends JFrame implements ProjectFinishedListener, Sing
 		});
 	}
 
-	/**
-	 * Launch the application.
-	 */
-	public static void start(String projectPath) {
+	@Override
+	public void scenarioInterrupted(final Scenario scenario, final int scenariosLeft) {
 		EventQueue.invokeLater(() -> {
-			FlatDarkLaf.setup();
-			// show GUI
-			ProjectViewModel model = new ProjectViewModel();
-			ProjectView frame = new ProjectView(model);
-			frame.setProjectSpecificActionsEnabled(false);
-			frame.setVisible(true);
-			frame.setSize(1200, 800);
-
-			frame.setIconImage(Toolkit.getDefaultToolkit()
-					.getImage(ProjectView.class.getResource("/icons/vadere-icon.png")));
-			if (projectPath.equals("")) {
-				frame.openLastUsedProject(model);
-			} else {
-				frame.openProject(model, projectPath);
-			}
-			checkDependencies(frame);
+			replace(scenario, VadereState.INTERRUPTED);
+			setScenariosRunning(false);
+			selectCurrentScenarioRunManager();
+			logger.info(String.format("all running scenarios interrupted"));
 		});
 	}
 
@@ -236,13 +226,33 @@ public class ProjectView extends JFrame implements ProjectFinishedListener, Sing
 	}
 	// ####################### End Part of the control ##################
 
-	@Override
-	public void scenarioInterrupted(final Scenario scenario, final int scenariosLeft) {
+	/**
+	 * Launch the application.
+	 */
+	public static void start(String projectPath){
 		EventQueue.invokeLater(() -> {
-			replace(scenario, VadereState.INTERRUPTED);
-			setScenariosRunning(false);
-			selectCurrentScenarioRunManager();
-			logger.info("all running scenarios interrupted");
+			try {
+				// Set Java L&F from system
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException
+					| IllegalAccessException e) {
+				IOUtils.errorBox("The system look and feel could not be loaded.", "Error setLookAndFeel");
+			}
+			// show GUI
+			ProjectViewModel model = new ProjectViewModel();
+			ProjectView frame = new ProjectView(model);
+			frame.setProjectSpecificActionsEnabled(false);
+			frame.setVisible(true);
+			frame.setSize(1200, 800);
+
+			frame.setIconImage(Toolkit.getDefaultToolkit()
+					.getImage(ProjectView.class.getResource("/icons/vadere-icon.png")));
+			if (projectPath.equals("")){
+				frame.openLastUsedProject(model);
+			} else {
+				frame.openProject(model, projectPath);
+			}
+			checkDependencies(frame);
 		});
 	}
 
@@ -265,7 +275,7 @@ public class ProjectView extends JFrame implements ProjectFinishedListener, Sing
 	private void openLastUsedProject(final ProjectViewModel model) {
 		String lastUsedProjectPath =
 				VadereConfig.getConfig().getString("History.lastUsedProject");
-		if (lastUsedProjectPath != null && !lastUsedProjectPath.isBlank()) {
+		if (lastUsedProjectPath != null && lastUsedProjectPath.isBlank() == false) {
 			if (Files.exists(Paths.get(lastUsedProjectPath))) {
 				ActionLoadProject.loadProjectByPath(model, lastUsedProjectPath);
 			}
