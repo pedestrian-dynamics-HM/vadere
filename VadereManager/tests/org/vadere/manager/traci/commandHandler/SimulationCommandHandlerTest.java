5388cff8ed5bb0aa8609cd5fca656bd7a1dc3ab4
Ludwig Jäck <ludwig.jaeck@hm.edu>
2022-09-19 16:08:30 +0200

    updated windows hash

--- VadereManager/tests/org/vadere/manager/traci/commandHandler/SimulationCommandHandlerTest.java
+++ VadereManager/tests/org/vadere/manager/traci/commandHandler/SimulationCommandHandlerTest.java
@@ -48,7 +48,7 @@ public class SimulationCommandHandlerTest extends CommandHandlerTest {
 		int varID = var.id;
 		TraCIDataType varType = var.type;
 		String elementID = "-1";
-		String retValWin = "17faa2749e83689e891a38197216a55dd18eb03b";
+		String retValWin = "6a81c4357c1639d13c00609915125f8b1ba76518";
 		String retValLin = "ac5468570f9165d503dd913aef66ab8f7fa06b47";
 
 		// response ok //
