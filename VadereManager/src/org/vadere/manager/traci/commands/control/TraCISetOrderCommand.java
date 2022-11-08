f27efc85b029782c01740891786a724c792b26fc
Lars Wischhof <wischhof@ieee.org>
2022-11-08 15:50:39 +0100

    add command class for handling SET_ORDER TraCI commands

---
+++ VadereManager/src/org/vadere/manager/traci/commands/control/TraCISetOrderCommand.java
@@ -0,0 +1,19 @@
+package org.vadere.manager.traci.commands.control;
+
+import org.vadere.manager.traci.TraCICmd;
+import org.vadere.manager.traci.commands.TraCICommand;
+import org.vadere.manager.traci.reader.TraCICommandBuffer;
+import org.vadere.manager.traci.writer.TraCIPacket;
+
+public class TraCISetOrderCommand extends TraCICommand {
+
+	public TraCISetOrderCommand(TraCICommandBuffer cmdBuffer) {
+		super(TraCICmd.SET_ORDER);
+	}
+
+	@Override
+	public TraCIPacket buildResponsePacket() {
+		return null;
+	}
+    
+}
