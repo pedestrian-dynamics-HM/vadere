abfb2ea1f48ed26eeae006743df07678a642b659
Hayato Hess <gasi04hi@hm.edu>
2026-02-25 16:49:31 +0100

    Remove unused waiting area id from AttributesCrossingTimeProcessor

--- VadereState/src/org/vadere/state/attributes/processor/AttributesCrossingTimeProcessor.java
+++ VadereState/src/org/vadere/state/attributes/processor/AttributesCrossingTimeProcessor.java
@@ -4,14 +4,4 @@ package org.vadere.state.attributes.processor;
  * @author Benedikt Zoennchen
  */
 public class AttributesCrossingTimeProcessor extends AttributesAreaProcessor {
-	private int waitingAreaId = -1;
-
-	public int getWaitingAreaId() {
-		return waitingAreaId;
-	}
-
-	public void setWaitingAreaId(int waitingAreaId) {
-		checkSealed();
-		this.waitingAreaId = waitingAreaId;
-	}
 }
